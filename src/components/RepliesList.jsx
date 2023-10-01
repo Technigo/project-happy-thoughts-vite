c04317413051365454e89f4f0352b329ca7b1064
So Youn Choi <manghatuhyo@gmail.com>
2023-10-01 20:04:15 +0100

    made box to messages

--- src/components/RepliesList.jsx
+++ src/components/RepliesList.jsx
@@ -1,13 +1,19 @@
 export const RepliesList = ({repliesProp}) => {
+    const repliesBox = {
+        border: '1px solid #ddd',
+        padding: '10px',
+        margin: '10px 0',
+        borderRadius: '5px',
+        boxShadow: '0 2px 5px rgba(0,0,0,0.1)',
+        backgroundColor: '#f9f9f9'
+    }
     if (!repliesProp) {
         return <p>Loading happy thoughts...</p>
     }
-    // const cleanReplies = repliesProp.message
     return (
-        // ${cleanReplies}
         <ul>
             {repliesProp?.map((reply, index) => (
-                <li key={reply._id}>{reply.message}</li>
+                <li key={reply._id} style={repliesBox}>{reply.message}</li>
             ))}
         </ul>
         )
