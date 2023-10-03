474a00a023aca5f46fe6a2330b67145e0be11f9b
Sakura Tanaka <sito6496t@google.com>
2023-10-03 18:34:41 +0200

    responsive is added

--- src/Components/MainSection/MainSection.jsx
+++ src/Components/MainSection/MainSection.jsx
@@ -7,33 +7,40 @@ import styles from "./MainSection.module.css";
 
 export const MainSection = () => {
   const [posts, setPosts] = useState([]);
-  const [error, setError] = useState({ error: false, message: "" });
+  const [error, setError] = useState(false);
   const [isLoad, setIsLoad] = useState(false);
 
   useEffect(() => {
+    // loading msaage is shown
     setIsLoad(true);
+    setError(false);
     const fetchData = async () => {
       try {
         const response = await fetch("https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts");
         if (!response.ok) throw new Error("Could not get data");
         const data = await response.json();
+
+        // to show loading message longer
         const setData = setTimeout(setPosts(data), 5000);
         return () => clearTimeout(setData);
       } catch (error) {
         console.error("Error fetching data:", error);
-        setError({ error: true, message: "Something went wrong with fetching data" });
+        setError(true);
       } finally {
         setIsLoad(false);
       }
     };
 
     fetchData();
+
+    // get api updated
     const intervalId = setInterval(fetchData, 100000);
 
     return () => {
       clearInterval(intervalId); // Clean up the interval when the component unmounts.
     };
   }, []);
+
   return (
     <main className={styles.main}>
       <Form onPosts={setPosts} />
