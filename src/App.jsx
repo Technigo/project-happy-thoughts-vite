e1ef720bfc75fc57ef0e8fc8eb2a36626e88b4c3
Carolina Luna <kroluna@gmail.com>
2023-10-03 16:03:03 +0200

    header color

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,27 @@
+import { useState, useEffect } from "react";
+import { Thoughts } from "./components/Thoughts";
+
 export const App = () => {
-  return <div>Find me in src/app.jsx!</div>;
+  const [thoughts, setThoughts] = useState([]);
+
+  useEffect(() => {
+    fetch(`GET https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts`)
+      .then((response) => response.json())
+      .then((data) => {
+        setThoughts(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching thoughts:", error);
+      });
+  }, []);
+
+  return (
+    <div className="App">
+      <div className="header">
+        <h1 className="original-gradient">Project Happy Thoughts</h1>
+        <h2>By CaroL @Technigo Bootcamp</h2>
+      </div>
+      <Thoughts thoughts={thoughts} setThoughts={setThoughts} />
+    </div>
+  );
 };
--- src/components/Thought.jsx
+++
@@ -1,3 +0,0 @@
-export const Thought = () => {
-  return <div>Thought</div>;
-};
