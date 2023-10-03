2e22546072d53a38a394b7892971c86de7055466
Andrej <72137459+andrkavr@users.noreply.github.com>
2023-10-03 12:02:57 +0200

    added useEffect to track total characters

--- src/components/writeAPost/WriteAPost.jsx
+++ src/components/writeAPost/WriteAPost.jsx
@@ -1,20 +1,42 @@
 import "./writeapost.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export const WriteAPost = () => {
   const [totalCharacters, setTotalCharacters] = useState(0);
+
+  const [exceedLimit, setExceedLimit] = useState(false);
+
+  useEffect(() => {
+    if (totalCharacters > 140) {
+      setExceedLimit(true);
+    } else if (totalCharacters <= 140) {
+      setExceedLimit(false);
+    }
+  }, [totalCharacters]);
 
   return (
     <div className="editor-container">
       <h2>What is making you happy right now?</h2>
       <textarea
-        style={{ color: totalCharacters > 140 ? "red" : "grey" }}
         type="text"
         className="comment-box"
         placeholder="Share your heart and warm others with your glow"
         onChange={(e) => setTotalCharacters(e.target.value.length)}
       />
-      <p className="charCount">{totalCharacters}/140</p>
+      <div className="postInfo">
+        <p
+          className="messageTooLong"
+          style={{ display: exceedLimit ? "flex" : "none" }}
+        >
+          Your message is too long 😔
+        </p>
+        <p
+          className="charCount"
+          style={{ color: exceedLimit ? "red" : "grey" }}
+        >
+          {totalCharacters}/140
+        </p>
+      </div>
     </div>
   );
 };
