c5173ac17256200d93932d0b2c05eab3a8ff78b8
Andrej <72137459+andrkavr@users.noreply.github.com>
2023-10-03 11:34:39 +0200

    added character count for post length

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { WriteAPost } from "./components/writeAPost/WriteAPost";
 export const App = () => {
   return (
     <div className="container">
+      <h1>Project Happy Thoughts</h1>
       <WriteAPost />
     </div>
   );
--- src/components/writeAPost/WriteAPost.jsx
+++ src/components/writeAPost/WriteAPost.jsx
@@ -1,18 +1,19 @@
 import "./writeapost.css";
+import { useState } from "react";
+
 export const WriteAPost = () => {
+  const [totalCharacters, setTotalCharacters] = useState(0);
+
   return (
     <div className="editor-container">
-      <h1>Project Happy Thoughts</h1>
-      {/* <input
-        type="text"
-        className="comment-box"
-        placeholder="Share your heart and warm others with your glow"
-      /> */}
+      <h2>What is making you happy right now?</h2>
       <textarea
         type="text"
         className="comment-box"
         placeholder="Share your heart and warm others with your glow"
+        onChange={(e) => setTotalCharacters(e.target.value.length)}
       />
+      <p className="charCount">{totalCharacters}/140</p>
     </div>
   );
 };
