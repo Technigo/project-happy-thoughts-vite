3f863c28e69c176510392d1e28424eae1e8f02bd
So Youn Choi <manghatuhyo@gmail.com>
2023-10-04 08:05:43 +0100

    placeholder

--- src/components/RepliesInput.jsx
+++ src/components/RepliesInput.jsx
@@ -2,7 +2,7 @@ export const RepliesInput = ({newReplies, onNewRepliesChange, onFormSubmit }) =>
   return(
     <form onSubmit={onFormSubmit}>
       <h1>what makes you happy</h1>
-      <textarea value={newReplies} onChange={onNewRepliesChange}></textarea>
+      <textarea placeholder="exchange thoughts and have hint how others doing" value={newReplies} onChange={onNewRepliesChange}></textarea>
       <button type="submit">send happy thought</button>
     </form>
   )
