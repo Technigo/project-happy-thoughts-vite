efe26cbaaf75f16083a16605bfb79ffe4737ccf0
So Youn Choi <manghatuhyo@gmail.com>
2023-10-04 13:48:10 +0100

    counting become red if char is more than 140

--- src/components/RepliesInput.jsx
+++ src/components/RepliesInput.jsx
@@ -4,7 +4,8 @@ export const RepliesInput = ({newReplies, onNewRepliesChange, onFormSubmit }) =>
     <form onSubmit={onFormSubmit}>
       <h1>what makes you happy</h1>
       <textarea placeholder="exchange thoughts and have hint how others doing" value={newReplies} onChange={onNewRepliesChange}></textarea>
-      <p>{newReplies.length}/140</p>
+      {/* <p className={`${newReplies.length >= 140 ? "red" : ""}`}>{newReplies.length}/140</p> */}
+      <p style={{ color: newReplies.length >=140 ? 'red' : 'inherit' }}>{newReplies.length}/140</p>
       <button type="submit">send happy thought</button>
     </form>
     </fieldset>
