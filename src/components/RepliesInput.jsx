510bf6e9f1722c355f2385fd398e691f36f8bfd8
So Youn Choi <manghatuhyo@gmail.com>
2023-10-04 15:22:13 +0100

    show short and cant long

--- src/components/RepliesInput.jsx
+++ src/components/RepliesInput.jsx
@@ -1,10 +1,32 @@
+import { useState, useEffect } from "react"
+
 export const RepliesInput = ({newReplies, onNewRepliesChange, onFormSubmit }) => {
+
+  const [errorMessage, setErrorMessage] = useState("")
+
+  useEffect(() => {
+    if (newReplies.length >= 141) {
+      setErrorMessage("too long")
+    } else {
+      setErrorMessage("")
+    }
+  }, [newReplies])
+
+  useEffect(() => {
+    if (newReplies.length < 4) {
+      setErrorMessage("too short")
+    } else {
+      setErrorMessage("")
+    }
+  }, [newReplies])
+
   return(
     <fieldset>
     <form onSubmit={onFormSubmit}>
       <h1>what makes you happy</h1>
       <textarea placeholder="exchange thoughts and have hint how others doing" value={newReplies} onChange={onNewRepliesChange}></textarea>
       {/* <p className={`${newReplies.length >= 140 ? "red" : ""}`}>{newReplies.length}/140</p> */}
+      <p>{errorMessage}</p>
       <p style={{ color: newReplies.length >=140 ? 'red' : 'inherit' }}>{newReplies.length}/140</p>
       <button type="submit">send happy thought</button>
     </form>
