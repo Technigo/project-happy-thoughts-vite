efe15059da2a5d61553c0c3585cd919594d7cc6d
majoh23 <139242342+majoh23@users.noreply.github.com>
2023-10-05 16:20:05 +0200

    Just basic css.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import ThoughtsList from './components/ThoughtsList';
 import { PostThought } from "./components/PostThought";
+import './App.css';
 
 export const App = () => {
-  return <div>
+  return <div className='main-wrapper'>
     
     <h1>Project Happy Thoughts</h1>
     <h2>Testing</h2>
