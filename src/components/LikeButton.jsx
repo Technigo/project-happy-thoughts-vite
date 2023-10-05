e29413306083ba8767544b15bbbed8349677a856
Carolina Luna <kroluna@gmail.com>
2023-10-05 16:27:40 +0200

    added likes, styling for buttons and forms, responsivness

---
+++ src/components/LikeButton.jsx
@@ -0,0 +1,16 @@
+// eslint-disable-next-line react/prop-types
+export const LikeButton = ({ id, hearts, onLike }) => {
+  const handleLike = () => {
+    // Call the function passed as prop when the button is clicked
+    onLike(id);
+  };
+
+  return (
+    <div className="like-btn-wrapper">
+      <button className="btn-style" onClick={handleLike}>
+        ❤️
+      </button>
+      <span className="like-num">x {hearts}</span>
+    </div>
+  );
+};
--- src/components/Thoughts.jsx
+++ src/components/Thoughts.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { LikeButton } from "./LikeButton";
 
 export const Thoughts = () => {
   const [thoughts, setThoughts] = useState([]);
@@ -14,12 +15,39 @@ export const Thoughts = () => {
       });
   }, []);
 
+  const handleLike = (id) => {
+    fetch(
+      `https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts/${id}/like`,
+      {
+        method: "POST",
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        const updatedThoughts = thoughts.map((thought) => {
+          if (thought._id === data._id) {
+            return data;
+          } else {
+            return thought;
+          }
+        });
+        setThoughts(updatedThoughts);
+      })
+      .catch((error) => {
+        console.error("Error liking thought:", error);
+      });
+  };
+
   return (
     <div>
       {thoughts.map((thought) => (
-        <div key={thought._id}>
-          <p>{thought.message}</p>
-          {/* Add other thought properties here if needed */}
+        <div key={thought._id} className="thought-box">
+          <p className="thought-text">{thought.message}</p>
+          <LikeButton
+            id={thought._id}
+            hearts={thought.hearts}
+            onLike={handleLike}
+          />
         </div>
       ))}
     </div>
