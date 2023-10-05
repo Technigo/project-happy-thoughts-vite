1966b1567caee41f6a0f8662c80eff2b9beef673
Isa Robertini <robertiniisa@gmail.com>
2023-10-05 20:43:02 +0200

    store comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./index.css";
 import "./components/styleForm.css";
 import { HeaderText } from "./components/header.jsx";
@@ -6,19 +6,30 @@ import { PostMessage } from "./components/PostMessage";
 import { MessageList } from "./components/MessageList";
 
 export const App = () => {
-  const [messages, setMessages] = useState([]); // State to store messages
-
+  // Initialize messages state with messages from local storage
+  const [messages, setMessages] = useState(
+    JSON.parse(localStorage.getItem("messages")) || []
+  );
 
   const addNewMessage = (message) => {
-    setMessages([...messages, message]); // Add the new message to the list
+    // Add the new message to the list
+    const newMessages = [...messages, message];
+    setMessages(newMessages);
+
+    // Save the updated messages to local storage
+    localStorage.setItem("messages", JSON.stringify(newMessages));
   };
 
-  //fetch like button and initialize count 
+  // Like button handler
   const handleLike = async (thoughtId) => {
     try {
-      await fetch(`https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts/${thoughtId}/like`, {
-        method: "POST",
-      });
+      await fetch(
+        `https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts/${thoughtId}/like`,
+        {
+          method: "POST",
+        }
+      );
+      // Update the likes count for the specific message
       const updatedMessages = messages.map((message) => {
         if (message._id === thoughtId) {
           return {
@@ -29,16 +40,23 @@ export const App = () => {
         return message;
       });
 
+      // Update state with the updated messages
       setMessages(updatedMessages);
+
     } catch (error) {
       console.error("Error:", error);
     }
   };
+
   return (
     <>
       <HeaderText />
       <PostMessage newMessage={addNewMessage} />
-      <MessageList messages={messages} handleLike={handleLike} /> {/* Passes the messages as a prop */}
+      <MessageList
+        messages={messages}
+        handleLike={handleLike}
+      />{" "}
+      {/* Pass the messages as a prop */}
     </>
   );
 };
