305857a938a80b905439bd53a9c2c35aae27e4f7
Emmy Jansson <emmybengtsson@live.se>
2023-10-05 08:48:48 +0200

    fixed the text in the box

--- src/HappyThought.jsx/NewThoughts.jsx
+++ src/HappyThought.jsx/NewThoughts.jsx
@@ -15,10 +15,12 @@ export const NewThoughts = () => {
     <div className="newThoughtsContainer">
       <h2>What makes you happy right now?</h2>
       <form onSubmit={handleSubmit}>
-        <input
+        <textarea
           type="text"
           value={newThoughts}
           onChange={(e) => setNewThoughts(e.target.value)}
+          rows="3"
+          maxLength="140"
           placeholder='"Most folks are about as happy as they make up their minds to be." -Abraham Lincoln'
           className="inputField"
         />
