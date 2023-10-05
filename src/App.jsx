729cab407ecde27a4babfdee40f32f5f3d0d77d3
hamdiolad <oladhamdi@gmail.com>
2023-10-05 23:24:28 +0200

    added components

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,16 @@
+import { Header } from "./components/header/Header";
+import { PostThought } from "./components/PostThought/PostThoughts";
+import { ThoughtCard } from "./components/PostThought/PostThoughts";
+
+
+//api url
+const API = "https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts";
+
+
+
+
+
+
 export const App = () => {
   return <div>Find me in src/app.jsx!</div>;
 };
