7e3b31ec201eda6871277af00e78b26c0b1b20ec
hamdiolad <oladhamdi@gmail.com>
2023-10-06 11:51:40 +0200

    fetching thoughts in app

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Header } from "./components/header/Header";
 import { PostThought } from "./components/PostThought/PostThoughts";
 import { ThoughtCard } from "./components/PostThought/PostThoughts";
 import "./app.css"
+import { useEffect } from "react";
 
 
 //api url
@@ -9,8 +10,27 @@ const API = "https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts";
 
 
 
+const fectchThought = async () => {
+  await fetch(API)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      // Handle successful response data
+    })
+    .catch(error => {
+      // Handle error here, e.g., display an error message to the user
+      console.error('Error:', error);
+    });
 
+};
 
+useEffect(() => {
+  fectchThought();
+}, [])
 
 export const App = () => {
   return (
