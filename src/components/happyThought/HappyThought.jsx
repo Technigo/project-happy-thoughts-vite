34ef06f201f1af23c66aeeaa8ec6cd8e1f9f8c08
Andrej <72137459+andrkavr@users.noreply.github.com>
2023-10-06 15:57:37 +0200

    added like update

--- src/components/happyThought/HappyThought.jsx
+++ src/components/happyThought/HappyThought.jsx
@@ -1,11 +1,35 @@
 import "./happythought.css";
 import { TimeAgo } from "../TimeAgo";
+import { useState } from "react";
 
 export const HappyThought = ({ thought }) => {
+  const [liked, setLiked] = useState(false);
+  const [totalHearts, setTotalHearts] = useState(thought.hearts);
   const thoughts = thought;
-  const handleLike = () => {
-    alert("You have liked this post!");
+  const handleLike = async (e) => {
+    // alert("You have liked this post!");
+    e.preventDefault();
+    if (!liked) {
+      setLiked(true);
+      const options = {
+        method: "POST", // Specifying the request method as POST
+        // Setting the content type of the request to application/json
+        headers: { "Content-Type": "application/json" },
+      };
+
+      await fetch(
+        `https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts/${thought._id}/like`,
+        options
+      )
+        .then((response) => response.json()) // Parsing the response as JSON
+        .then((data) => {
+          setTotalHearts(data.hearts);
+        })
+        // Logging any errors that occur during the fetch operation
+        .catch((error) => console.log(error));
+    }
   };
+
   return (
     <>
       {thoughts ? (
@@ -14,11 +38,15 @@ export const HappyThought = ({ thought }) => {
           <p className="post-text">{thought.message}</p>
           <div className="like-and-time">
             <div className="like-data">
-              <button onClick={handleLike} className="likeBtn">
+              <button
+                onClick={handleLike}
+                className="likeBtn"
+                style={{ backgroundColor: !liked ? "pink" : "#ee8bb4" }}
+              >
                 <span className="like-heart-emoji">🧡</span>
               </button>
               {/* <p className="total-likes">x0</p> */}
-              <p className="total-likes">x {thought.hearts}</p>
+              <p className="total-likes">x {totalHearts}</p>
             </div>
             {/* <p className="time-of-post">15 mins ago</p> */}
             <p className="time-of-post">
