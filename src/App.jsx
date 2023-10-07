4e7bef68376de2a9e70b3c82d028a5382aff95ee
Amanda Rambert Hammarström <amanda.rambert@gmail.com>
2023-10-07 13:16:59 +0200

    Fetched post data and made small changes in code

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,63 @@
 import Container from "./components/container/Container";
 import Post from "./components/post/Post";
 import Form from "./components/form/Form";
+import { useEffect, useState } from "react";
 
 export const App = () => {
+    const [posts, setPosts] = useState([]);
+    const [error, setError] = useState(false);
+    const [loading, setLoading] = useState(true);
+
+    // This is a React Hook that is executed when the component is mounted
+    useEffect(() => {
+        // This is an asynchronous function to fetch data from a server
+        const getPosts = async () => {
+            try {
+                // It sends a GET request to this URL
+                const result = await fetch("https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts");
+                // It waits for the response and converts it to JSON format
+                const data = await result.json();
+
+                // It sets the state of 'posts' in the component with modified data
+                setPosts(data.map((post) => ({
+                    id: post._id,
+                    message: post.message,
+                    hearts: post.hearts,
+                    createdAt: post.createdAt,
+                })));
+            } catch {
+                // If there's an error during fetching, this block of code is executed
+                setError(true);
+            } finally {
+                // This block of code is executed no matter what, to stop loading state
+                setLoading(false);
+            }
+        };
+
+        // This line calls the function 'getPosts' defined above
+        getPosts();
+
+        // The empty array here [] means this effect will only run when the component mounts
+    }, []);
+
     return (
         <Container>
             <Form />
-            <Post
-                message="Hola babe"
-                hearts={4}
-                createdAt="10 seconds ago"
-            />
+            {loading && (
+                <p>Loading posts ...</p>
+            )}
+            {error && (
+                <p>Could not get posts</p>
+            )}
+            {!loading && !error && posts.map((post) => (
+                <Post
+                    id={post.id}
+                    key={post.id}
+                    message={post.message}
+                    hearts={post.hearts}
+                    createdAt={post.createdAt}
+                />
+            ))}
         </Container>
     );
 };
--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,16 +1,24 @@
-import Button from "../button/Button";
-import Card from "../card/Card";
-import styles from "./Post.module.css";
+// Importing necessary libraries and components
+import dayjs from "dayjs";  // A library for parsing, formatting, and manipulating dates
+import relativeTime from "dayjs/plugin/relativeTime";  // Plugin for dayjs to handle relative time
+import Button from "../button/Button";  // A custom Button component
+import Card from "../card/Card";  // A custom Card component
+import styles from "./Post.module.css";  // CSS module for styling
 
+// Extending dayjs with the relativeTime plugin
+dayjs.extend(relativeTime);
+
+// Defining the React component 'Post' using a functional component
 const Post = ({
     id,
     message,
     hearts,
     createdAt,
 }) => {
+    // This is the JSX code that defines what the component will render
     return (
         <Card className={styles.card}>
-            <p>{message}</p>
+            <p className={styles.message}>{message}</p>
             <div className={styles.footer}>
                 <div>
                     <Button>
@@ -20,10 +28,11 @@ const Post = ({
                         {`x ${hearts}`}
                     </span>
                 </div>
-                <span className={styles.time}>{createdAt}</span>
+                <span className={styles.time}>{dayjs(createdAt).fromNow()}</span>
             </div>
         </Card>
     );
 }
 
+// Exporting the component as the default export of this module
 export default Post;
