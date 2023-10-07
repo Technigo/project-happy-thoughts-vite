a8aec3e2cc96ca30a996facaaafe2356c118bebf
veronica astrom <v.aastrom@gmail.com>
2023-10-07 11:01:57 +0200

    updated singleelement component

--- src/components/SingleMessage.jsx
+++ src/components/SingleMessage.jsx
@@ -12,24 +12,30 @@ export const SingleMessage = ({ singleMessage, fetchPosts }) => {
     const messageId = singleMessage._id;
 
     const onLikeIncrease = async () => {
-
-        const options = {
-            method: "POST", // Specifying the request method as POST
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({messageId}) 
-        };
-        await fetch(
-            `https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts/${messageId}/like`,
-            options
-        )
-            .then((response) => response.json()) // Parsing the response as JSON
-            .then((data) => {
-                setLiked(true);
-                setNumLikes(data.hearts);
-            })
-            // Logging any errors that occur during the fetch operation
-            .catch((error) => console.log(error));
+        try {
+            const options = {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ messageId }),
+            };
+    
+            const response = await fetch(
+                `https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts/${messageId}/like`,
+                options
+            );
+    
+            if (!response.ok) {
+                throw new Error('Failed to like the message');
+            }
+    
+            const data = await response.json();
+            setLiked(true);
+            setNumLikes(data.hearts);
+        } catch (error) {
+            console.error('Error:', error);
+        }
     };
+    
 
 
     return <section className="single-message-wrapper">
@@ -51,6 +57,7 @@ export const SingleMessage = ({ singleMessage, fetchPosts }) => {
 // Explanation:
 // This SingleMessage component is designed to display individual messages from an API and manage the liking functionality. It renders a message, a like button, the number of likes, and the time elapsed since the message was posted, calculated using moment.js.
 
+
 //When a user clicks the like button, a POST request is sent to the API to increment the like count for that specific message, the local like count state (numLikes) is updated, and the fetchPosts function is called to refresh the message list. The component also visually indicates whether a message has been liked by the user by changing the color of the like button.
 // Hint This component does not use the useEffect hook at all ;)
 // It's a POST method :)
@@ -58,7 +65,6 @@ export const SingleMessage = ({ singleMessage, fetchPosts }) => {
 // Here is a hint of that function :)
 // const onLikeIncrease = async () => {
 //   // Defining options for the fetch API call, specifying that the method should be "POST"
-//   // PROMISE LAND;)
 //   // - Making a POST request to the API to like a message, using the message's `_id` property to target the correct message
 //   // - Parsing the response from the API as JSON
 //   // - Updating the `numLikes` and `liked` state variables and fetching the updated posts
