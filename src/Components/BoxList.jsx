29b8dedf5f42450fd15358becee5a78e92a44e33
Minadarabi <minaadabirii@gmail.com>
2023-10-08 01:08:04 -0700

    added time

--- src/Components/BoxList.jsx
+++ src/Components/BoxList.jsx
@@ -1,6 +1,40 @@
 /* eslint-disable react/prop-types */
 
 
+const formatDate = (date) => {
+    const currentDate = new Date();
+    const currentTimestamp = currentDate.getTime();
+    const currentSeconds = Math.floor(currentTimestamp / 1000);
+
+    const oldDate = new Date(date)
+    const oldTimestamp = oldDate.getTime();
+    const oldSeconds = Math.floor(oldTimestamp / 1000);
+
+
+    const difference = currentSeconds - oldSeconds;
+    let output = ``;
+    if (difference < 60) {
+        // Less than a minute has passed:
+        output = `${difference} seconds ago`;
+    } else if (difference < 3600) {
+        // Less than an hour has passed:
+        output = `${Math.floor(difference / 60)} minutes ago`;
+    } else if (difference < 86400) {
+        // Less than a day has passed:
+        output = `${Math.floor(difference / 3600)} hours ago`;
+    } else if (difference < 2620800) {
+        // Less than a month has passed:
+        output = `${Math.floor(difference / 86400)} days ago`;
+    } else if (difference < 31449600) {
+        // Less than a year has passed:
+        output = `${Math.floor(difference / 2620800)} months ago`;
+    } else {
+        // More than a year has passed:
+        output = `${Math.floor(difference / 31449600)} years ago`;
+    }
+    return output
+}
+
 export const BoxList = ({info , like}) => {
 
 
@@ -15,7 +49,7 @@ export const BoxList = ({info , like}) => {
                 <button className="like-btn" onClick={() => like(info._id)}><span className="emoji">❤️</span></button>
                 <span className="like-number">x {info.hearts}</span>
             </div>
-            <div className="time">{info.createdAt}</div>
+            <div className="time">{formatDate(info.createdAt)}</div>
         </div>
 
     </div>
