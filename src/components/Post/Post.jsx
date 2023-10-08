59573895303be12b71098acaf4118f8c05982e81
Emmy Dieden <emmydieden@Emmys-MacBook-Pro.local>
2023-10-08 08:55:58 +0200

    added overflow-wrap: break-word

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -7,16 +7,18 @@ import React from 'react'
 export const Post = ({post}) => {
      const { _id, message, hearts, createdAt } = post //destructuring the post 
      
-    //  console.log(_id)
-     
 
      return(
-         <div className={`post-wrapper${post[0]? "latest" : "" }`}>
+        <>
+         <div className="post-wrapper">
+            <div>
              <p>{message}</p>
+             </div>
              <div className="heart-time">
                 <HeartBtn id={_id} hearts={hearts}/>
                 {moment(createdAt).fromNow()}
              </div>
          </div>
+         </>
      )
 }
