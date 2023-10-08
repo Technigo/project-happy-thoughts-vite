03e6832834b64e8ffd604afafd2fd7da94aa5b5b
Susanne Ekenheim <susanne.ekenheim@gmail.com>
2023-10-08 14:51:58 +0200

    icons failed, testing

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ export const Footer = () => {
     <div className="footer-container">
       <h3>by Susanne Ekenheim</h3>
       <a href="https://github.com/smExlex" target="blank">
-        <img src="public/github-logo.svg" alt="Github icon" />
+        <img src="./github-logo.svg" alt="Github icon" />
       </a>
       <a href="https://www.linkedin.com/in/susanne-e-6915a087/" target="blank">
         <img src="public/linkedin.svg" alt="LinkedIn icon" />
