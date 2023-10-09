17eab67b62929c979011a85fd147a1d545bf06c9
Sheryll-Ann <shey.pc@gmail.com>
2023-10-09 20:34:06 +0200

    displaced github icon

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ export const Footer = () => {
         <div className="footer-container">
             <p>Created by </p>
             <a href="https://github.com/Sheryll-Ann" target="blank">
-                <img src="src/assets/icons8-github.png" alt="Github icon" />
+                <img src="./icons8-github.png" alt="Github icon" />
             </a>
             <p>Sheryll-Ann Poo-Cheong</p>
 
