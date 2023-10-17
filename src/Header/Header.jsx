12e3207a4c61ade23fd6f52585583896963e0dd4
Shiqwan <ulrika.galnander@gmail.com>
2023-10-17 19:47:18 +0200

    First commits a lot to figure out

---
+++ src/Header/Header.jsx
@@ -0,0 +1,10 @@
+export const Header = () => {
+  return (
+    <div className="header">
+      <h1 className="week-title">Project Happy Thoughts</h1>
+      <h2>Week 7</h2>
+    </div>
+  );
+};
+
+export default Header;
