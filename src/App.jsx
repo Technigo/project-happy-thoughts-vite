ab63bf0054821fb26c7b534c087a619d96b4549a
Linnea Johansson <Linneajoh@gmail.com>
2023-10-29 21:11:20 +0100

    fetch thoughts

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,36 @@
+import { useState, useEffect } from "react";
+import { Header } from "./Components/Header";
+
+
 export const App = () => {
-  return <div>Find me in src/app.jsx!</div>;
+  const [thoughts, setThoughts] = useState([]);
+  
+  const [error, setError] = useState(null);
+  
+  const handleNewThought = (newThought) => {
+    setThoughts([newThought, ...thoughts]);
+  };
+
+  const fetchThoughts = async () => {
+    try {
+      const response = await fetch('https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts');
+      const data = await response.json();
+      setThoughts(data);
+    } catch (error) {
+      console.error('Error fetching thoughts', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchThoughts();
+  }, []);
+  
+  
+  return (
+    <div className='main-wrapper'>
+      <Header />
+      <NewThoughts onNewThought={handleNewThought} />
+      <ThoughtList thoughts={thoughts} fetchThoughts={fetchThoughts} />
+    </div>
+  );
 };
---
+++ src/Components/Header.jsx
@@ -0,0 +1,9 @@
+import "../index.css"
+
+export const Header = () => {
+    return (
+        <div className="header-wrapper">
+            <h1>Project Happy Thoughts</h1>
+        </div>
+    )
+}
