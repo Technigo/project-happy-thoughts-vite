901a3a30b5efe79df97d3559b8d5d94baade1849
psterner <psterner@hotmail.com>
2023-11-01 14:46:08 +0100

    Create component for likes

---
+++ src/assets/components/LikeMessage.jsx
@@ -0,0 +1,15 @@
+/**
+ * Create a like button
+ */
+
+// POST <https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts/THOUGHT_ID/like>
+
+// When the user clicks the heart button on a thought, send a POST request (with no body)
+// to this URL. Replace THOUGHT_ID with the _id parameter of the thought the user clicked on
+
+// less than a minute ago
+// 1 minute ago
+// 6 minutes ago
+// 10 minutes ago
+// about 1 hour ago
+// about 2 hours ago
