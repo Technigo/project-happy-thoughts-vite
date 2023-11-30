5a03a90d0de57ee93402848ad34afae730e3f689
sheryll.poo-cheong <shey_pc@gmail.com>
2023-11-30 21:43:29 +0100

    connecting 20msg post request to backend api

--- src/components/Distributive.jsx
+++ src/components/Distributive.jsx
@@ -3,67 +3,59 @@ import { useState, useEffect } from "react";
 // Importing `PostMessage` and `MessageList` components from respective files
 import { PostMessage } from "./PostMessage";
 import { MessageList } from "./MessageList";
-import './Distributive.css'
-
+import "./Distributive.css";
 
 // Declaring a functional component named `Distributive`
 export const Distributive = () => {
+  // Declaring state `loading` and its updater function `setLoading`, initializing it with `false`
+  const [loading, setLoading] = useState(false);
 
-    // Declaring state `loading` and its updater function `setLoading`, initializing it with `false`
-    const [loading, setLoading] = useState(false);
+  // Declaring state `messageList` and its updater function `setMessageList`, initializing it with an empty array
+  const [messageList, setMessageList] = useState([]);
 
-    // Declaring state `messageList` and its updater function `setMessageList`, initializing it with an empty array
-    const [messageList, setMessageList] = useState([]);
+  // Declaring a function `fetchPosts` to fetch posts from the API
+  const fetchPosts = () => {
+    const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
-    // Declaring a function `fetchPosts` to fetch posts from the API
-    const fetchPosts = () => {
+    // Setting `loading` state to `true` to indicate data fetching is in progress
+    setLoading(true);
 
-        // Setting `loading` state to `true` to indicate data fetching is in progress
-        setLoading(true);
+    // Making a GET request to the API endpoint
+    fetch(`${backendUrl}/thoughts`)
+      // Parsing the response as JSON
+      .then((response) => response.json())
+      // Updating `messageList` state with the parsed data
+      .then((data) => setMessageList(data))
+      // Logging any errors that occur during the fetch operation
+      .catch((error) => console.error(error))
+      // Setting `loading` state to `false` once data fetching is complete
+      .finally(() => setLoading(false));
+  };
 
-        // Making a GET request to the API endpoint
-        fetch("https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts")
+  // Using `useEffect` hook to call `fetchPosts` once when the component mounts (due to the empty dependency array `[]`)
+  useEffect(() => {
+    fetchPosts();
+  }, []);
 
-            // Parsing the response as JSON
-            .then((response) => response.json())
-            // Updating `messageList` state with the parsed data
-            .then((data) => setMessageList(data))
-            // Logging any errors that occur during the fetch operation
-            .catch((error) => console.error(error))
-            // Setting `loading` state to `false` once data fetching is complete
-            .finally(() => setLoading(false));
-    };
+  // Declaring a function `addNewPost` to update `messageList` state with a new message
+  const addNewPost = (newMessage) => {
+    // Updating `messageList` state by adding `newMessage` at the beginning of the array
+    setMessageList([newMessage, ...messageList]);
+  };
 
-    // Using `useEffect` hook to call `fetchPosts` once when the component mounts (due to the empty dependency array `[]`)
-    useEffect(() => {
-        fetchPosts();
-    }, []);
+  // Returning JSX to render the component UI
+  return (
+    <div className="main-wrapper">
+      {/* Rendering `PostMessage` component and passing `addNewPost` and `fetchPosts` as props */}
+      <PostMessage newMessage={addNewPost} fetchPosts={fetchPosts} />
 
-    // Declaring a function `addNewPost` to update `messageList` state with a new message
-    const addNewPost = (newMessage) => {
-        // Updating `messageList` state by adding `newMessage` at the beginning of the array
-        setMessageList([newMessage, ...messageList]);
-    };
-
-    // Returning JSX to render the component UI
-    return (
-        <div className="main-wrapper" >
-            {/* Rendering `PostMessage` component and passing `addNewPost` and `fetchPosts` as props */}
-            < PostMessage
-                newMessage={addNewPost}
-                fetchPosts={fetchPosts}
-            />
-
-            {loading ? <p
-                className="loading"
-            >Loading... ⏳</p> :
-                <MessageList
-                    messageList={messageList}
-                    fetchPosts={fetchPosts}
-                />}
-
-        </div>
-    );
+      {loading ? (
+        <p className="loading">Loading... ⏳</p>
+      ) : (
+        <MessageList messageList={messageList} fetchPosts={fetchPosts} />
+      )}
+    </div>
+  );
 };
 
 // Explanation:
