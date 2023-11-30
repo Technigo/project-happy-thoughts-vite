f4199739a167eeb6a07c60c7bba31faf6dd90fe2
sheryll.poo-cheong <shey_pc@gmail.com>
2023-11-30 21:42:15 +0100

    connecting hearts increment to backend api

--- src/components/SingleMessage.jsx
+++ src/components/SingleMessage.jsx
@@ -1,65 +1,64 @@
-import { formatDistance } from 'date-fns'
-import './SingleMessage.css'
+import { formatDistance } from "date-fns";
+import "./SingleMessage.css";
 
 export const SingleMessage = ({ singleMessage, fetchPosts }) => {
-
-    const onLikeIncrease = async (thoughts_id) => {
-
-
-        // Defining options for the fetch API call, specifying that the method should be "POST"
-        const options = {
-            // Specifying the request method as POST
-            method: "POST",
-            // Setting the content type of the request to application/json
-            headers: { "Content-Type": "application/json" },
-        };
-
-        // PROMISE LAND;)
-        // - Making a POST request to the API to like a message, using the message's `_id` property to target the correct message
-        // - Parsing the response from the API as JSON
-        // - Updating the `numLikes` and `liked` state variables and fetching the updated posts
-        // - Logging any errors that occur during the fetch operation to the console
-        // post request:
-        try {
-            const response = await fetch(
-                `https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts/${thoughts_id}/like`,
-                options
-            );
-            // Not processing the data returned from API, as a GET request through fetchPosts() is carried out afterwards, so not needed/unactivated:
-            // const data = await response.json();
-
-            // Calling `fetchPosts` function (passed as prop) to re-fetch posts
-            fetchPosts();
-        }
-        // Logging any errors that occur during the fetch operation
-        catch (error) {
-            console.log(error)
-        }
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  const onLikeIncrease = async () => {
+    // Defining options for the fetch API call, specifying that the method should be "POST"
+    const options = {
+      // Specifying the request method as POST
+      method: "POST",
+      // Setting the content type of the request to application/json
+      headers: { "Content-Type": "application/json" },
     };
-    return (
-        <>
-            <p>{singleMessage.message}</p>
-            <div className="heart-time-container">
-                <div className="heart-like">
-                    <button
-                        type="button"
-                        className="heart-btn"
-                        id='heartBtn'
-                        onClick={() => onLikeIncrease(singleMessage._id)}
-                    >
-                        <span className="heart-icon" aria-label='like button'>❤️</span>
-                    </button>
-                    <span className="number-likes"> x {singleMessage.hearts}</span>
-                </div>
-                <span className="time-passed"> {
-                    formatDistance(
-                        new Date(singleMessage.createdAt),
-                        Date.now(),
-                        { addSuffix: true }
-                    )} </span>
-            </div>
-        </ >
-    );
+
+    // PROMISE LAND;)
+    // - Making a POST request to the API to like a message, using the message's `_id` property to target the correct message
+    // - Parsing the response from the API as JSON
+    // - Updating the `numLikes` and `liked` state variables and fetching the updated posts
+    // - Logging any errors that occur during the fetch operation to the console
+    // post request:
+    try {
+      const response = await fetch(
+        `${backendUrl}/thoughts/${singleMessage._id}/like`,
+        options
+      );
+      // Not processing the data returned from API, as a GET request through fetchPosts() is carried out afterwards, so not needed/unactivated:
+      // const data = await response.json();
+
+      // Calling `fetchPosts` function (passed as prop) to re-fetch posts
+      fetchPosts();
+    } catch (error) {
+      // Logging any errors that occur during the fetch operation
+      console.log(error);
+    }
+  };
+  return (
+    <>
+      <p>{singleMessage.message}</p>
+      <div className="heart-time-container">
+        <div className="heart-like">
+          <button
+            type="button"
+            className="heart-btn"
+            id="heartBtn"
+            onClick={() => onLikeIncrease()}
+          >
+            <span className="heart-icon" aria-label="like button">
+              ❤️
+            </span>
+          </button>
+          <span className="number-likes"> x {singleMessage.hearts}</span>
+        </div>
+        <span className="time-passed">
+          {" "}
+          {formatDistance(new Date(singleMessage.createdAt), Date.now(), {
+            addSuffix: true,
+          })}{" "}
+        </span>
+      </div>
+    </>
+  );
 };
 
 // Explanation:
