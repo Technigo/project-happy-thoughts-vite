4e0cce6d73d812c237866d8ac0865c2357db5efe
Malin <malinlundhe@hotmail.com>
2023-12-03 20:37:25 +0100

    added new url

--- src/components/apiService.jsx
+++ src/components/apiService.jsx
@@ -6,7 +6,7 @@ import '../index.css';
 
 
 
-const API_URL = "https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts";
+const API_URL = "https://happy-thoughts-api-8tgx.onrender.com/";
 
 export const fetchThoughts = async () => {
 try {
