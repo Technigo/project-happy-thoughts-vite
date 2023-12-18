adc7459a096c637c382866e1d1dc657362c15936
Emmy Dieden <emmydieden@Emmys-MacBook-Pro.local>
2023-12-18 18:38:10 +0100

    fixed loader and margins

--- src/App.jsx
+++ src/App.jsx
@@ -1,91 +1,93 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 
-import { Post } from './components/Post/Post'
-import { Create } from './components/Create/Create'
+import { Post } from "./components/Post/Post";
+import { Create } from "./components/Create/Create";
 
 export const App = () => {
-  const [posts, setPosts] = useState([])
-  const [newPost, setNewPost] = useState("")
-  const [loading, setLoading] = useState(true)
-  const [error, setError] = useState(null)
-  
+  const [posts, setPosts] = useState([]);
+  const [newPost, setNewPost] = useState("");
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
   // const API = "https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts"
-  const API = "https://happy-thoughts-emmy-dieden.onrender.com/thoughts"
+  const API = "https://happy-thoughts-emmy-dieden.onrender.com/thoughts";
 
   //-------------Fetching the messages-------------------
-  
-    const fetchPosts = async () => {
-      try {
-        const response = await fetch(API)
 
-        if(!response.ok) {
-          throw new Error('Failed to fetch data')
-        }
+  const fetchPosts = async () => {
+    try {
+      const response = await fetch(API);
 
-        const posts = await response.json() //parse the response as JSON
-        setPosts(posts) //Update the state with fetched posts
-        console.log(posts)
-        setLoading(false) //Set loading to false as data fetching is complete
-      } catch (error) {
-        console.error ('Error fetching data:', error)
-        setError(error)
-        setLoading(false)
+      if (!response.ok) {
+        throw new Error("Failed to fetch data");
       }
-      
-      
+
+      const posts = await response.json(); //parse the response as JSON
+      setPosts(posts); //Update the state with fetched posts
+      console.log(posts);
+      setLoading(false); //Set loading to false as data fetching is complete
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      setError(error);
+      setLoading(false);
     }
+  };
 
   //----------useEffect to initiate data fetch when component is mounted----------
-    useEffect(()=> {
-      fetchPosts() 
+  useEffect(() => {
+    fetchPosts();
 
-      //Function that fetches the latest API every 5 seconds.
-      const intervalFetching = setInterval(fetchPosts, 5000) 
-        return () => {
-          clearInterval(intervalFetching) // Clean up the interval when the component unmounts.
-    }
-    }, []) //UseEffect depends on an empty array, so runs only once
+    //Function that fetches the latest API every 5 seconds.
+    const intervalFetching = setInterval(fetchPosts, 5000);
+    return () => {
+      clearInterval(intervalFetching); // Clean up the interval when the component unmounts.
+    };
+  }, []); //UseEffect depends on an empty array, so runs only once
   //------------------------------------------------------------------------
-   
 
   //-----------Rendering Loading/error messages: ----------------
-  if(loading){
-    return (
-      <div className="loader-wrapper">
-    <div className="loader"></div>
-    <div>Loading thoughts...</div>
-    </div>)
+  // if (loading) {
+  //   return (
+  //     <div className="loader-wrapper">
+  //       <div className="loader"></div>
+  //       <div>Loading thoughts...</div>
+  //     </div>
+  //   );
+  // }
+  if (error) {
+    return <div>Error: {error.message}</div>;
   }
-  if(error){
-    return <div>Error: {error.message}</div>
-  }
-//-----------------------------------------------------------------
-
+  //-----------------------------------------------------------------
 
   //-----------Function to update posts-----------------------
   const addNewPost = (newPost) => {
-    //Updating 'posts' state by adding 'newPost' at the beginning of the array: 
-    setPosts([newPost, ...posts])
-  }
+    //Updating 'posts' state by adding 'newPost' at the beginning of the array:
+    setPosts([newPost, ...posts]);
+  };
   //----------------------------------------------------------
 
-
-  
   return (
     <div className="main-section">
-            <h1>Happy thoughts</h1>
-            <Create 
-              addNewPost={addNewPost}
-              fetchPosts={fetchPosts}
-              newPost ={newPost}
-              setNewPost ={setNewPost}
-              />
-            <div> {/*Rendering all the posts with a map() */}
-              {posts.map((post) => (
-                <Post post={post} key={post._id}/>
-              )) }
-              
-            </div>
+      <h1>Happy thoughts</h1>
+      <Create
+        addNewPost={addNewPost}
+        fetchPosts={fetchPosts}
+        newPost={newPost}
+        setNewPost={setNewPost}
+      />
+      {loading ? (
+        <div className="loader-wrapper">
+          <div className="loader"></div>
+          <div>Loading thoughts...</div>
+        </div>
+      ) : (
+      <>
+          {/* Rendering all the posts with a map() */}
+          {posts.map((post) => (
+            <Post post={post} key={post._id} />
+          ))}
+        </>
+      )}
     </div>
-  )
+  );
 };
