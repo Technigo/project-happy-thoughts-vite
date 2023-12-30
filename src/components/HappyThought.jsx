afc8436e0839c1906ea52804fac18d0b3e224340
Olga Lepistö <olga.lepisto@gmail.com>
2023-12-30 23:24:58 +0200

    fixed the issue with message box height

--- src/components/HappyThought.jsx
+++ src/components/HappyThought.jsx
@@ -28,6 +28,3 @@ const handleLike = () => {
         </div>
     )
 }
-
-// API for liking a post `POST <https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts/THOUGHT_ID/like`>.
-// Replace "THOUGHT_ID" with relevant _id parameter
