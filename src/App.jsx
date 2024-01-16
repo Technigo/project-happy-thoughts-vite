3e1a3a6337e8e0c4519c9d27518710dfad2be9bf
Olga Lepistö <olga.lepisto@gmail.com>
2024-01-16 13:24:48 +0200

    css fixes

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,11 @@ import { Feed } from './components/Feed.jsx'
 export const App = () => {
   return (
     <div id="app-container">
+      <div id="content-wrapper">
     <Header />
     <Form />
     <Feed />
+    </div>
     </div>
   )
 }
