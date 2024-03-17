66811479a096345148eed5e3fe9354696e9d2471
ohitsnathalie <152981339+ohitsnathalie@users.noreply.github.com>
2024-03-17 12:25:43 +0100

    creating css files and connecting them to their jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Header } from "./components/Header.jsx"
 import { Form } from "./components/Form.jsx"
 import { Feed } from "./components/Feed.jsx"
-
+import "./styling/app.css"
 
 export const App = () => {
   return <div>Find me in src/app.jsx!</div>
---
+++ src/components/Header.jsx
@@ -0,0 +1,1 @@
+import "./styling/header.css"
