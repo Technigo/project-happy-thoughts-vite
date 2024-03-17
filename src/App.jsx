8571fd65406653cdcccb67b0189426c7c94c5d2c
ohitsnathalie <152981339+ohitsnathalie@users.noreply.github.com>
2024-03-17 13:20:10 +0100

    notes on what to do

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,13 @@ import { Form } from "./components/Form.jsx"
 import { Feed } from "./components/Feed.jsx"
 import "./styling/app.css"
 
+
+// Define state and default value
+// Connect API and return data for the feed
+// Create a thought/ connecting with Form.jsx
+// Implementing Like system
+
+
 export const App = () => {
   return <div>Find me in src/app.jsx!</div>
 }
