a4109d8fed36f061c1f91372c75d446ea1fd3099
ohitsnathalie <152981339+ohitsnathalie@users.noreply.github.com>
2024-03-17 12:12:10 +0100

    addes some components and connected to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,8 @@
+import { Header } from "./components/Header.jsx"
+import { Form } from "./components/Form.jsx"
+import { Feed } from "./components/Feed.jsx"
+
+
 export const App = () => {
-  return <div>Find me in src/app.jsx!</div>;
-};
+  return <div>Find me in src/app.jsx!</div>
+}
