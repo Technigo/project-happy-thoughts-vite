7357af2a7751cf78ae12d386699127175537debb
Wendy Zhao <wendy.w.zhao@outlook.com>
2024-03-18 17:06:44 +0100

    Create message and thoughtcard component files

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header";
+import Message from "./components/Message";
 
 export const App = () => {
   return (
     <>
       <Header />
+      <Message />
     </>
   );
 };
---
+++ src/components/ThoughtCard.jsx
@@ -0,0 +1,11 @@
+const ThoughtCard = ({ message, likes, time }) => {
+  return (
+    <div>
+      <p>{message}</p>
+      <p>{likes}</p>
+      <p>{time}</p>
+    </div>
+  );
+};
+
+export default ThoughtCard;
