cf293a09f2fd32bea4528deb82169fe5da904aba
fridaforser <fridaforser@gmail.com>
2024-03-19 14:51:43 +0100

    Tested out a basic component structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,25 @@
+import { useState, useEffect } from "react";
+import { HappyThoughts } from "./components/Thoughts";
+
 export const App = () => {
-  return <div>Find me in src/app.jsx!</div>;
+  const [thoughts, setThoughts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts")
+      .then((res) => res.json())
+      .then((json) => {
+        setThoughts(json);
+        console.log(json);
+      });
+  }, []);
+
+  useEffect(() => {
+    console.log("Testing", thoughts);
+  }, [thoughts]);
+
+  return (
+    <div>
+      <HappyThoughts thought={thoughts} />
+    </div>
+  );
 };
