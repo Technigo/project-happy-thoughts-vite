44a36df90bf12f7bc4f1aab60ed8c96261c73cf6
Tejpex <equis_angel@hotmail.com>
2024-03-19 15:17:40 +0100

    Attempted to change how time is displayed

--- src/components/Thoughts/TimeStamp.jsx
+++ src/components/Thoughts/TimeStamp.jsx
@@ -1,7 +1,19 @@
+import { useState, useEffect } from "react"
 import PropTypes from "prop-types"
 
 export const TimeStamp = ({ time }) => {
+    const [timePosted, setTimePosted] = useState()
+    const timeNow = new Date()
 
+    useEffect(() => {
+        setTimePosted(new Date({time}))
+    }, []);
+    
+    /*useEffect(() => {
+      console.log("Posted", timePosted)
+      console.log("Now", timeNow);
+    }, [timePosted]);*/
+    
   return (
     <div className="time-box">
       <p>Posted {time}</p>
