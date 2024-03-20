7d5a94421d5613d0d9393fd07ab40b4ad481bfa6
Johanna Catalini Smith <johannacatalini@gmail.com>
2024-03-20 20:49:06 +0100

    add day js

--- src/Post.jsx
+++ src/Post.jsx
@@ -1,5 +1,16 @@
-export const Post = (props) => {
+// import opensoruce date library (to get the time right), instead of writing the code by myself. https://day.js.org/docs/en/plugin/relative-time
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+dayjs.extend(relativeTime);
 
-  
-  return <div> {props.message} {props.hearts} {props.createdAt} </div>;
+export const Post = (props) => {
+  return (
+    <div>
+      {" "}
+      {props.message} {props.hearts}
+      {" "}
+      {dayjs().to(dayjs(props.createdAt))}
+      <hr></hr>
+    </div>
+  );
 };
