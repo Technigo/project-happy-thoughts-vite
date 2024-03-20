205c643ab3115760edd28727419d741450abdf8b
fridaforser <fridaforser@gmail.com>
2024-03-20 15:24:02 +0100

    Added the .map function here instead to be able to create boxes for each thought. Also added functionality to create a new post

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import { useState, useEffect } from "react";
 import { HappyThoughts } from "./components/Thoughts";
+import { PostThoughts } from "./components/PostThought";
 
 export const App = () => {
   const [thoughts, setThoughts] = useState([]);
+  const [newThought, setNewThought] = useState("");
 
+  // FETCHING THE VALUES FROM THE API
   useEffect(() => {
     fetch("https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts")
       .then((res) => res.json())
@@ -13,13 +16,45 @@ export const App = () => {
       });
   }, []);
 
-  useEffect(() => {
-    console.log("Testing", thoughts);
-  }, [thoughts]);
+  // FUNCTION TO HANDLE THE NEW POST
+  const handleNewPost = (event) => {
+    setNewThought(event.target.value);
+  };
 
+  const handlePostSubmit = (event) => {
+    event.preventDefault();
+    fetch("https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts", {
+      method: "POST",
+      body: JSON.stringify({
+        message: newThought,
+      }),
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((res) => res.json())
+      .then((newThought) => {
+        setThoughts((previousThoughts) => [newThought, ...previousThoughts]);
+      });
+  };
+
+  // APP SECTION TO SHOWCASE THE INPUT FIELD AND THE OTHER THOUGHTS
   return (
-    <div>
-      <HappyThoughts thought={thoughts} />
+    <div className="app-section">
+      <h1>Happy thoughts</h1>
+      <PostThoughts
+        postSubmit={handlePostSubmit}
+        newMessage={handleNewPost}
+        newThought={newThought}
+      />
+      <div className="thoughts-section">
+        {thoughts.map((thought) => (
+          <HappyThoughts
+            key={thought._id}
+            message={thought.message}
+            likes={thought.hearts}
+            time={thought.createdAt}
+          />
+        ))}
+      </div>
     </div>
   );
 };
