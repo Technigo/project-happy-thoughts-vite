d4fa826969c351c2093c28b4c7b6a29062096f9c
Johanna Billingskog Nyberg <johanna@billingskognyberg.se>
2024-03-20 12:26:50 +0100

    Fixed issue with components not rendering correctly.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+// App.jsx
 import React from "react";
 import { Message } from "./ShowMessage";
 import { MessageInput } from "./MessageInput";
 
 const App = () => {
+  const handleNewMessage = (newMessage) => {
+    // Handle new message, if needed
+    console.log("New message:", newMessage);
+  };
+
   return (
     <div className="App">
-      <MessageInput />
+      <MessageInput onNewMessage={handleNewMessage} />
       <Message />
     </div>
   );
