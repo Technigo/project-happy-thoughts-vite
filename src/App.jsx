b1a1455043a60e28c707897125fb7e5658b235f1
unknown <linda.frischknecht@bluewin.ch>
2024-03-20 12:04:26 +0100

    added input box and button, push not done yet

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import { Input } from "./components/Input";
 import { Thought } from "./components/Thought";
 
 export const App = () => {
   return (
     <div className="app">
+      <Input />
       <Thought />
     </div>
   );
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,3 +1,24 @@
-//import { useState, useEffect } from "react";
+import { useEffect } from "react";
+import "./Input.css";
 
-export const Input = () => {};
+export const Input = () => {
+  //const url = ...
+
+  const handleUserInput = async () => {};
+
+  useEffect(() => {
+    handleUserInput();
+  }, []);
+
+  return (
+    <div>
+      <form className="input-box">
+        What is making you happy right now?
+        <input className="input-field" type="text"></input>
+        <button className="button-input" type="submit">
+          Send Happy Thought
+        </button>
+      </form>
+    </div>
+  );
+};
