000111731e99c3fa816d16e88bc8691f26b4cd08
Johanna Catalini Smith <johannacatalini@gmail.com>
2024-03-20 20:59:20 +0100

    add use state for likes (button)

--- src/Likes.jsx
+++ src/Likes.jsx
@@ -1,7 +1,12 @@
-
+import { useState } from "react";
 
 export const Likes = (props) => {
-  return <button>{props.hearts}</button> 
+
+  const [likes, setLikes] = useState(props.hearts);
+// here we make the function to make the heart (button) clickable
+  const onClick = () => {
+    setLikes(likes + 1);
+  };
+
+  return <button onClick={onClick}>{likes}</button>;
 };
-
-
