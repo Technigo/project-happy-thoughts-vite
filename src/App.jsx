10983059cce18f823e8dbf7465c6c6ec7ae15959
Your Name <you@example.com>
2024-03-20 18:27:47 +0100

    Messages showing on the DOM, Some styling made, save and tomorrow more

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,33 @@
-export const App = () => {
-  return <div>Find me in src/app.jsx!</div>;
+import { HappyCard } from "./components/HappyCard";
+import { useEffect, useState } from "react"
+
+
+export const App  = () =>{  
+  const [thoughts, setThoughts] = useState ([])
+
+  useEffect (() => {
+    fetchThoughts()
+  }, [])
+
+  const fetchThoughts = async () => {
+    try{
+      const response = await fetch ("https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts")
+      const data = await response.json()
+      setThoughts(data)
+    } catch (error) {
+      console.error("Error fetching thoughts:", error)
+    }
+  }
+
+  return (
+    <div className="App">
+      {thoughts.map(thoughts => (
+      <HappyCard 
+        key={thoughts.id}
+        message={thoughts.message}
+        hearts={thoughts.hearts}
+        createdAt={thoughts.createdAt}
+      />))}      
+    </div>
+  )
 };
