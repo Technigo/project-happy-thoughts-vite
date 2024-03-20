bc38092b4020020bb19c04fcdeb7ed8c334d3321
Alma Herrström <almaherrstrom@Almas-MacBook-Air-4.local>
2024-03-20 10:52:46 +0100

    added API endpoint to fetch 20 thoughts

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,29 @@
+import { useEffect, useState } from "react"
+import "./App.css"
+
 export const App = () => {
-  return <div>Find me in src/app.jsx!</div>;
-};
+  //Initialize state for storing dafault data
+  const [thoughts, setThoughts] = useState([]) //Thought data
+
+  //Define API endpoints
+  const thoughtsURL = "https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts"
+
+  //Function to fetch data from API
+  const fetchHappyThoughts = () => {
+    fetch(thoughtsURL)
+      .then((result) => result.json())
+      .then((json) => {
+        console.log(json)
+        setThoughts(json)
+      })
+    //Handle errors here
+  }
+
+  //Fetch thoughts when component is mounted
+  useEffect(() => {
+    fetchHappyThoughts()
+  }, [])
+
+  //Return stuff
+  return <div className="delete">Find me in src/app.jsx!</div>
+}
