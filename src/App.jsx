1f580bd7d039e2a3541133175596a8894c7a8531
ohitsnathalie <152981339+ohitsnathalie@users.noreply.github.com>
2024-03-20 22:09:27 +0100

    fixing some code

--- src/App.jsx
+++ src/App.jsx
@@ -1,90 +1,67 @@
+import { useEffect, useState } from "react"
 import { Header } from "./components/Header.jsx"
 import { Form } from "./components/Form.jsx"
 import { Feed } from "./components/Feed.jsx"
 import "./styling/app.css"
-import { useEffect, useState } from "react"
-
-
-// Create a thought/ connecting with Form.jsx
-// Implementing Like system
-
 
 export const App = () => {
-// Define state and default value
-  const [thought, setThought] = useState([])
-  const [totalLikes, setTotalLikes] = useState(
-    parseInt(localStorage.getItem("totalLikes")) || 0
-  )
+	// Define state and default value
+	const [thought, setThought] = useState([])
+	const [totalLikes, setTotalLikes] = useState(
+		parseInt(localStorage.getItem("totalLikes")) || 0
+	)
 
-// Connect API and fetch data for the feed
-    const API = "https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts"
+	// Connect API and fetch data for the feed
+	const API = "https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts"
 
-    const fetchData = async () => {
-      
-      const startTime = Date.now();
+	const fetchData = async () => {
+		try {
+			const response = await fetch(API)
+			if (!response.ok) {
+				throw new Error("Failed to fetch happy thoughts")
+			}
+			const data = await response.json()
+			setThought(data)
+		} catch (error) {
+			console.error("Error fetching data", error)
+		}
+	}
 
-      try {
-        const response = await fetch(API)
-        if (!response.ok) {
-          throw new Error("Failed to fetch happy thoughts")
-        }
-        const data = await response.json()
-        setThought(data)
-      } catch (error) {
-        console.error("Error fetching data", error)
-      } finally {
-        const elapsedTime = Date.now() - startTime
-        const remainingTime = Math.max(0, minimumLoadingTime - elapsedTime);
+	useEffect(() => {
+		fetchData()
+		//Timer to fetch data every 5 seconds
+		const idInterval = setInterval(fetchData, 5000)
+		return () => {
+			clearInterval(idInterval)
+		}
+	}, [])
 
-        setTimeout(() => {
-          setLoading(false);
-          console.log("Success fetching data");
-        }, remainingTime);
-      } 
-    }
+	// useEffect hook to save the total likes to localStorage
+	useEffect(() => {
+		localStorage.setItem("totalLikes", totalLikes.toString())
+	}, [totalLikes])
 
-    useEffect(() => {
-      fetchData()
-      //Timer to fetch data every 5 seconds
-      const timeInterval = setTimeInterval(fetchData, 5000)
-      return () => {
-        clearInterval(timeInterval)
-      }
-    }, [])
+	const addThought = (newThought) => {
+		const thoughtKey = Date.now() //using time as key
+		const thoughtAndKey = {
+			...newThought,
+			_id: thoughtKey,
+		}
+		setThought([thoughtAndKey, ...thought])
+	}
 
-    // useEffect hook to save the total likes to localStorage
-    useEffect(() => {
-      localStorage.setItem("totalLikes", totalLikes.toString())
-    }, [totalLikes])
-
-    const addThought = (newThought) => {
-      const thoughtKey = Date.now() //using time as key
-      const thoughtAndKey = {
-        ...newThought,
-        _id: thoughtKey,
-      }
-      setThought([thoughtAndKey, ...thought])
-    }
-
-  return (
-    <>
-      <Header />
-      <div className="main-wrapper">
-        <Form
-          newThought={addThought}
-          apiURL={API}
-          fetchData={fetchData}
-          />
-          {loading ? (
-          <LoadingComp />
-          ):(
-            <Feed
-            thoughtData={thought}
-            onLikeChange={(likeChange) =>
-              setTotalLikes(totalLikes + likeChange)}
-              />
-          )}
-      </div>
-    </>
-  )
+	return (
+		<>
+			<Header />
+			<div className='main-wrapper'>
+				<Form newThought={addThought} apiURL={API} fetchData={fetchData} />
+				() : (
+				<Feed
+					thoughtData={thought}
+					onLikeChange={(likeChange) => setTotalLikes(totalLikes + likeChange)}
+				/>
+				)
+			</div>
+		</>
+	)
 }
