3702eb5202499f0df9e7a0d67bcb0f51c645fe55
Martin Jönsson <martin.t.joensson@gmail.com>
2024-03-20 17:34:59 +0100

    added prop validation to time function

--- src/components/Time.jsx
+++ src/components/Time.jsx
@@ -1,33 +1,39 @@
+import PropTypes from "prop-types";
+
 export const Time = (props) => {
-    const now = new Date();
-    const created = new Date(props.createdAt)
+  const now = new Date();
+  const created = new Date(props.createdAt);
 
-      function timeSince(created) {
-      var seconds = Math.floor((now - created) / 1000);
+  function timeSince(created) {
+    var seconds = Math.floor((now - created) / 1000);
 
-      var interval = seconds / 31536000;
+    var interval = seconds / 31536000;
 
-      if (interval > 1) {
-        return Math.floor(interval) + " years";
-      }
-      interval = seconds / 2592000;
-      if (interval > 1) {
-        return Math.floor(interval) + " months";
-      }
-      interval = seconds / 86400;
-      if (interval > 1) {
-        return Math.floor(interval) + " days";
-      }
-      interval = seconds / 3600;
-      if (interval > 1) {
-        return Math.floor(interval) + " hours";
-      }
-      interval = seconds / 60;
-      if (interval > 1) {
-        return Math.floor(interval) + " minutes";
-      }
-      return Math.floor(seconds) + " seconds";
+    if (interval > 1) {
+      return Math.floor(interval) + " years";
     }
+    interval = seconds / 2592000;
+    if (interval > 1) {
+      return Math.floor(interval) + " months";
+    }
+    interval = seconds / 86400;
+    if (interval > 1) {
+      return Math.floor(interval) + " days";
+    }
+    interval = seconds / 3600;
+    if (interval > 1) {
+      return Math.floor(interval) + " hours";
+    }
+    interval = seconds / 60;
+    if (interval > 1) {
+      return Math.floor(interval) + " minutes";
+    }
+    return Math.floor(seconds) + " seconds";
+  }
 
-    return <p>{timeSince(created)} ago</p>;
+  return <p>{timeSince(created)} ago</p>;
+};
+
+Time.propTypes = {
+  createdAt: PropTypes.string,
 };
