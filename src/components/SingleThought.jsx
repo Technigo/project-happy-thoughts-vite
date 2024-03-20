711f66b6f2c801589de5db0a2a609c65017f2fd9
ohitsnathalie <152981339+ohitsnathalie@users.noreply.github.com>
2024-03-20 19:00:19 +0100

    useEffect for one thought, to check the likes

--- src/components/SingleThought.jsx
+++ src/components/SingleThought.jsx
@@ -1,6 +1,15 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 export const SingleThought = ({ eachThought, onLikeChange }) => {
 	const [like, setLike] = useState(false)
 	const [numberLikes, setNumberLikes] = useState(eachThought.hearts)
+
+	const LIKE_API = `https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts/${eachThought._id}/like`
+
+	useEffect(() => {
+		const likedThought = JSON.parse(localStorage.getItem("likedThought")) || []
+		if (likedThought.includes(eachThought._id)) {
+			setLike(true)
+		}
+	}, [eachThought._id])
 }
