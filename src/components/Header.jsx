fb1296b7cbe6d60263b0359be1e9f3f5a686e92c
ohitsnathalie <152981339+ohitsnathalie@users.noreply.github.com>
2024-03-21 23:56:51 +0100

    changed the header title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import "../styling/header.css"
 export const Header = () => {
 	return (
 		<div className='header-wrapper'>
-			<h1>Project Header</h1>
+			<h1>Happy Thought Project</h1>
 		</div>
 	)
 }
