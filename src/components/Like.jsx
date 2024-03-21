e2216dae0ad323959d7fc7b058ab371a7a063b58
Erica Mechler <erica.mechler1@gmail.com>
2024-03-21 20:57:48 +0100

    added a click-state for the like button so that it turns more pink for a while once you have clicked on the button

--- src/components/Like.jsx
+++ src/components/Like.jsx
@@ -1,11 +1,25 @@
+import { useState } from "react";
+
 export const Like = ({ thoughtId, handleLike }) => {
+  const [clicked, setClicked] = useState(false);
   // Function for handling like action
   const handleLikeClick = () => {
+    // Call the handleLike function to handle the like action
     handleLike(thoughtId);
+
+    // Toggle the liked state
+    setClicked(true);
+    // Reset the clicked state after a short delay
+    setTimeout(() => {
+      setClicked(false);
+    }, 800);
   };
 
   return (
-    <button className="like-button" onClick={handleLikeClick}>
+    <button
+      className={`like-button ${clicked ? "clicked" : ""}`}
+      onClick={handleLikeClick}
+    >
       💗
     </button>
   );
