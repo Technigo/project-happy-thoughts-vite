1be14dfaedee5876ce036b4a4dadd881fe927c2d
Tejpex <equis_angel@hotmail.com>
2024-03-21 09:35:12 +0100

    Made site stop posting things on launch

--- src/components/PostMessage.jsx
+++ src/components/PostMessage.jsx
@@ -1,13 +1,13 @@
-import { useState, useEffect } from "react"
+import { useState } from "react"
 import PropTypes from "prop-types";
 
 export const PostMessage = ({showNewPost}) => {
-  const [submit, setSubmit] = useState(false);
+  //const [submit, setSubmit] = useState(false);
   const [message, setMessage] = useState("");
   const disableSubmit = message.length < 5 || message.length > 140;
 
   //Post a new thought when submit-state is changed
-  useEffect (() => {
+  const postThought = () => {
     fetch("https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts", {
       method: "POST",
       body: JSON.stringify({
@@ -20,13 +20,13 @@ export const PostMessage = ({showNewPost}) => {
         console.log(json)
         showNewPost(json)
       });
-  }, [submit]
-  )
+  }
 
   //Change submit-state when form is submitted
   const handlePostSubmit = (event) => {
-    event.preventDefault();
-    setSubmit(!submit)
+    event.preventDefault()
+    //setSubmit(!submit)
+    postThought()
   }
 
   return (
--- src/components/Thoughts/Likes.jsx
+++ src/components/Thoughts/Likes.jsx
@@ -1,12 +1,12 @@
-import { useState, useEffect } from "react"
+import { useState } from "react"
 import PropTypes from "prop-types"
 import HeartRed from "../../assets/heart-red.png"
 
 export const Likes = ({ id, likes }) => {
-  const [newLike, setNewLike] = useState(false)
+  //const [newLike, setNewLike] = useState(false)
   const [likesToShow, setLikesToShow] = useState(likes)
   
-  useEffect(() => {
+  const postLike = () => {
     fetch(`https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts/${id}/like`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
@@ -16,11 +16,11 @@ export const Likes = ({ id, likes }) => {
         console.log(json);
         setLikesToShow(likesToShow+1)
       });
-  }, [newLike])
+  }
 
   return (
     <div className="like-box">
-      <button onClick={() => setNewLike(!newLike)}>
+      <button onClick={postLike}>
         <img src={HeartRed} alt="Icon of a heart" className="heart"/>
       </button>
       <p>{likesToShow}</p>
