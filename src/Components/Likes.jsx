053617254303513ba067074860fb13dfe85561c4
fridaforser <fridaforser@gmail.com>
2024-03-21 14:06:51 +0100

    Created a like functionality where the like count and button styling changed when pressed

--- src/Components/Likes.jsx
+++ src/Components/Likes.jsx
@@ -1,8 +1,31 @@
-export const Likes = ({ likes }) => {
+import { useState } from "react";
+
+export const Likes = ({ id, likes }) => {
+  const [newLike, setNewLike] = useState(likes);
+
+  const heartLike = () => {
+    fetch(
+      `https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts/${id}/like`,
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+      }
+    )
+      .then((res) => res.json())
+      .then((json) => {
+        setNewLike(newLike + 1);
+      });
+  };
+
   return (
     <div className="likes">
-      <button className="heart-button">❤️</button>
-      <p className="likes">x {likes}</p>
+      <button
+        onClick={heartLike}
+        className={likes !== newLike ? "likedColorClass" : "notLikedClass"}
+      >
+        ❤️
+      </button>
+      <p className="likes">x {newLike}</p>
     </div>
   );
 };
