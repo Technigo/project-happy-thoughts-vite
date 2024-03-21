1f8ec104bc9f8ee2b3a889f94eb24a5bc26fe6e5
fridaforser <fridaforser@gmail.com>
2024-03-21 14:07:44 +0100

    Used the date fns functionality to create a "time since posted" function

--- src/Components/Time.jsx
+++ src/Components/Time.jsx
@@ -1,7 +1,13 @@
+import { formatDistanceStrict } from "date-fns";
+
 export const Time = ({ time }) => {
+  const postedWhen = formatDistanceStrict(new Date(time), new Date(), {
+    addSuffix: true,
+  });
+
   return (
     <div>
-      <p className="time">{time}</p>
+      <p className="time">{postedWhen}</p>
     </div>
   );
 };
