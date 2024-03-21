ada84a348b8e0adc32c98966b4e202e3bdc870b1
fridaforser <fridaforser@gmail.com>
2024-03-21 14:07:17 +0100

    Added an ID prop

--- src/Components/Thoughts.jsx
+++ src/Components/Thoughts.jsx
@@ -1,12 +1,12 @@
 import { Likes } from "./Likes.jsx";
 import { Time } from "./Time.jsx";
 
-export const HappyThoughts = ({ message, likes, time }) => {
+export const HappyThoughts = ({ id, message, likes, time }) => {
   return (
     <div className="box">
       <p className="message">{message}</p>
       <div className="message-details">
-        <Likes likes={likes} />
+        <Likes likes={likes} id={id} />
         <Time time={time} />
       </div>
     </div>
