fdda5a8d23c646da40a10e2f60d9a9aa249b8e85
Viktoria Olofsson <viktoriaolofsson@gmail.com>
2024-03-22 12:13:22 +0100

    validation and error message

--- src/components/NewThoughtForm.jsx
+++ src/components/NewThoughtForm.jsx
@@ -2,24 +2,43 @@ import { useState } from "react";
 
 export const NewThoughtForm = ({ setThoughts, fetchData, apiUrl }) => {
   const [message, setMessage] = useState("");
+  const [charactersUsed, setCharactersUsed] = useState(140);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [isRed, setIsRed] = useState(false);
 
   const handleChange = (event) => {
-    setMessage(event.target.value);
+    const newMessage = event.target.value;
+    setMessage(newMessage);
+    setCharactersUsed(() => 140 - newMessage.length);
+    if (newMessage.length > 140) {
+      setIsRed(true);
+    } else {
+      setIsRed(false);
+    }
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    fetch(apiUrl, {
-      method: "POST",
-      body: JSON.stringify({ message: message }),
-      headers: { "Content-Type": "application/json" },
-    })
-      .then((response) => response.json())
-      .then((newThought) => {
-        setThoughts((previousThoughts) => [newThought, ...previousThoughts]);
-        setMessage("");
-        fetchData();
-      });
+    if (message.length < 5) {
+      setErrorMessage("Thought must be longer than 5 characters");
+      return;
+    } else if (message.length > 140) {
+      setErrorMessage("Thought must not exceed 140 characters");
+      return;
+    } else {
+      fetch(apiUrl, {
+        method: "POST",
+        body: JSON.stringify({ message: message }),
+        headers: { "Content-Type": "application/json" },
+      })
+        .then((response) => response.json())
+        .then((newThought) => {
+          setThoughts((previousThoughts) => [newThought, ...previousThoughts]);
+          setMessage("");
+          setErrorMessage("");
+          setCharactersUsed(140);
+        });
+    }
   };
 
   return (
@@ -32,6 +51,10 @@ export const NewThoughtForm = ({ setThoughts, fetchData, apiUrl }) => {
         value={message}
         onChange={handleChange}
       />
+      <span className={isRed ? "red-message" : ""}>
+        {charactersUsed}/140 <span id="error-message">{errorMessage}</span>
+      </span>
+
       <label htmlFor="new-thought" className="visually-hidden">
         Write a happy thought
       </label>
