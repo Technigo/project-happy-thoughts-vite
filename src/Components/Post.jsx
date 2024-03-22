dcddde519560fd3469c1a5718c0e64eab409bd67
Paulajungaker <paula010514@hotmail.com>
2024-03-22 12:04:53 +0100

    Displaying how long ago the post was made

--- src/Components/Post.jsx
+++ src/Components/Post.jsx
@@ -1,10 +1,42 @@
 import PropTypes from "prop-types";
 
 const Post = ({ thought, onLike }) => {
-  const { message, hearts } = thought;
+  const { message, hearts, createdAt } = thought;
 
   const handleLike = () => {
     onLike(thought._id);
+  };
+
+  const calculateTimeDifference = (createdAt) => {
+    const currentTime = new Date();
+    const postTime = new Date(createdAt);
+    const timeDifference = Math.abs(currentTime - postTime) / 1000;
+
+    if (timeDifference < 5) {
+      return `${Math.round(timeDifference)} seconds ago`;
+    } else if (timeDifference < 10) {
+      return `10 seconds ago`;
+    } else if (timeDifference < 20) {
+      return `20 seconds ago`;
+    } else if (timeDifference < 30) {
+      return `30 seconds ago`;
+    } else if (timeDifference < 60) {
+      `${Math.round(timeDifference)} second${
+        Math.round(timeDifference) === 1 ? "" : "s"
+      } ago`;
+    } else if (timeDifference < 3600) {
+      return `${Math.round(timeDifference / 60)} minute${
+        Math.round(timeDifference / 60) === 1 ? "" : "s"
+      } ago`;
+    } else if (timeDifference < 21600) {
+      return `${Math.round(timeDifference / 3600)} hour${
+        Math.round(timeDifference / 3600) === 1 ? "" : "s"
+      } ago`;
+    } else {
+      return `${Math.round(timeDifference / 86400)} day${
+        Math.round(timeDifference / 86400) === 1 ? "" : "s"
+      } ago`;
+    }
   };
 
   return (
@@ -19,6 +51,7 @@ const Post = ({ thought, onLike }) => {
         </span>{" "}
       </button>
       <span className="likeCount">x {hearts}</span>
+      <span className="timeAgo">{calculateTimeDifference(createdAt)}</span>
     </div>
   );
 };
