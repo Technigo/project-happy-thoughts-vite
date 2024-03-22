dfcada51bc01979c695a1bdae91836e1723c4201
Sofia Johansson <153744881+sofia32057@users.noreply.github.com>
2024-03-22 15:07:34 +0100

    Update - error handling done

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,8 +1,10 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Counter } from "./Counter";
 
 export const Form = ({ newThought, setNewThought }) => {
   const [count, setCount] = useState(0);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [submitOk, setSubmitOk] = useState(false)
 
   const postThought = async event => {
     event.preventDefault();
@@ -16,13 +18,25 @@ export const Form = ({ newThought, setNewThought }) => {
         }
       );
       if (!response.ok) {
+        console.log("Response", response);
         throw new Error("Error fetching data");
       }
+      const data = response.json();
+      console.log("Data", data);
       setNewThought("");
     } catch (error) {
+      setErrorMessage("Your thought couldn't be posted...");
       throw new Error("Error", error);
     }
   };
+
+  useEffect(() => {
+    setTimeout(() => setErrorMessage(""), 5000);
+  }, [errorMessage]);
+
+  useEffect(() => {
+    count >= 5 && count <= 140 && setSubmitOk(true);
+  }, [count]);
 
   const handleChange = event => {
     setNewThought(event.target.value);
@@ -30,21 +44,26 @@ export const Form = ({ newThought, setNewThought }) => {
   };
 
   return (
-    <form
-      className="thought-form"
-      onSubmit={postThought}>
-      <label>
-        What&apos;s making you happy right now?
-        <textarea
-          name="thought"
-          id="thought-input"
-          cols="30"
-          rows="10"
-          value={newThought}
-          onChange={handleChange}></textarea>
-      </label>
-      {count > 0 && <Counter characters={count} />}
-        <button className="submit-btn">❤️ Send Happy Thought ❤️</button>
-    </form>
+      <form
+        className="thought-form"
+        onSubmit={postThought}>
+        <label>
+          What&apos;s making you happy right now?
+          <textarea
+            name="thought"
+            id="thought-input"
+            cols="30"
+            rows="10"
+            value={newThought}
+            onChange={handleChange}></textarea>
+        </label>
+        {errorMessage && <p className="error error-message">{errorMessage}</p>}
+        {count > 0 && <Counter characters={count} />}
+        <button
+          className="submit-btn"
+          disabled={!submitOk}>
+          ❤️ Send Happy Thought ❤️
+        </button>
+      </form>
   );
 };
