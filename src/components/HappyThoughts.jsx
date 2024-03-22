d05e1cc48efea357739dd7cd38ffc63b83ebfa25
Frida Svensson <fridamarisvensson@gmail.com>
2024-03-22 14:22:53 +0100

    define API endpoint and create function that fetches thoughts

--- src/components/HappyThoughts.jsx
+++ src/components/HappyThoughts.jsx
@@ -5,7 +5,16 @@ export const HappyThoughts = () => {
   // useStates for thoughts list, new thoughts and loading
 
   // fetch Thoughts
+  const url = "https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts";
 
+  const fetchThoughts = () => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json);
+      });
+  };
+  fetchThoughts();
   // POST new thought
 
   // fetch updated list with new thought included
--- src/components/ThoughtsForm.jsx
+++ src/components/ThoughtsForm.jsx
@@ -2,7 +2,7 @@ const ThoughtsForm = () => {
   return (
     // ---- submit form wrapper -----
     // What's making you happy right now?
-    <p>`What`&apos:`s making you happy right now?`</p>
+    <p>What&apos;s making you happy right now?</p>
     // input field
 
     // submit button
