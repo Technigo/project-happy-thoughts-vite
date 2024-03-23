35eaf9c165e19659092d8f1cbf4613e782dded5b
Trista Shan <trista8703@hotmail.com>
2024-03-23 14:58:16 +0200

    heart button works

--- src/component/GetMessage/GetThought.jsx
+++ src/component/GetMessage/GetThought.jsx
@@ -1,4 +1,5 @@
 import {useState, useEffect} from "react";
+// import { HeartButton } from "./HeartButton";
 // import { SingleThought } from "../singleThought";
 
 export const GetThought = () => {
@@ -6,7 +7,11 @@ export const GetThought = () => {
     const [loading, setLoading] = useState(true)
     const [newThoughts, setNewThoughts] = useState('')
     const [error, setError] = useState(null)
-    const [success, setSuccess] = useState(false)
+    // const [heartsCount, setHeartsCount] = useState()
+    
+    // const [heartsAmount, setHeartsAmount] = useState('')
+    // const [clickHeart, setClickHeart] = useState(0)
+    // const [success, setSuccess] = useState(false)
     // heart and its button => another component?
     //message length 
     //text box required
@@ -36,8 +41,12 @@ export const GetThought = () => {
         }
       }, [])
 
-      const handleFormSubmit = e => {
+      const handleFormSubmit = (e) => {
         e.preventDefault()
+       
+
+        if (newThoughts.length < 5 || newThoughts.length >140) {
+            setError('Please write 5 to 140 characters.')}
 
         fetch(URL,{
             method: 'POST',
@@ -48,18 +57,31 @@ export const GetThought = () => {
           .then((newThought) => {
             setNewThoughts('')
             setNewThoughts((oldThoughts)=> [newThought, ...oldThoughts])
-            setSuccess(true)
+            // setSuccess(true)
             setError(null)
           })
           .catch(error => {
             console.error('Error sending thought:', error)
             setError(error.message)
-            setSuccess(false)
+            // setSuccess(false)
           })
       }
 
-
-
+        // fetch(`https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts/${oldThoughts.id}/like`, {
+        //     method: 'POST',
+        //     headers: {'Content-Type': 'application/json'},
+        //     body: ({hearts: heartsAmount})
+        // })
+        //   .then(res => res.json())
+        //   .then(() => {
+        //     oldThoughts.map(oldThought => {
+        //         setHeartsAmount('')
+        //         {oldThought.id ? {...oldThought, hearts: heartsAmount} : oldThought}
+        //     },
+        //     setClickHeart(prev => prev + 1)
+        //     )
+        //   })
+      
       const timeFormat = (createdAt) => {
         const currentTimeSec = new Date ()/1000
         const postTimeSec = new Date (createdAt) /1000 //convert the time to second
@@ -80,25 +102,47 @@ export const GetThought = () => {
         } else { return timeDifferentDay === 1 ? `${timeDifferentDay} day ago` : `${timeDifferentDay} day ago`}
       }
 
+      const handleLikeThought = async (thoughtId) => {
+        try {
+            const res = await fetch (`https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts/${thoughtId}/like`,
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            if (res.ok) {
+                const updatedLikeThought = oldThoughts.map (oldThought => 
+                    oldThought._id === thoughtId ? {...oldThought, hearts: oldThought.hearts + 1} : oldThought
+                )
+                setOldThoughts(updatedLikeThought)
+            } else throw new Error ('Failed to like the thought')
+        } catch (error) {
+            console.error('Error liking the thought:', error)
+        }
+      }
+
       return(
         <>
         {error && <div>Error: {error}</div>}
-        {success && 
         <form onSubmit={handleFormSubmit}>
             <label >
                 <h2>What&apos;s making you happy right now?</h2>
                     <input 
                       type="text" 
                       name="text" 
+                      placeholder="Write your thought here"
+                      minLength={5}
+                      maxLength={140}
                       value={newThoughts} 
                       onChange={handleInputChange} 
-                      id="text" /> 
+                      id="text" />
             </label>
         <div className="submit-wrapper">
             <button >❤️ Send Happy Thought ❤️</button>
         </div>
         </form>
-        }
+        
     
 
         <div className="loadingContainer">
@@ -106,9 +150,12 @@ export const GetThought = () => {
         {oldThoughts.map((oldThought)=>{
           return(
             <>
-              <div className="oldThoughtContainer" key={oldThought.id}>
+              <div className="oldThoughtContainer" key={oldThought._id}>
                 <p>{oldThought.message}</p>
-                <p>❤️ {oldThought.hearts}</p>
+                <p>
+                    <span><button className="heart-button" onClick={() => handleLikeThought(oldThought._id)}>❤️</button></span> 
+                    {oldThought.hearts}
+                </p>
                 <p>{timeFormat(oldThought.createdAt)}</p>
              </div>
             </>
