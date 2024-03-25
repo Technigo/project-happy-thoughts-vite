24c1147ba039677f000eef718001ec1069fd3f9a
Johanna Billingskog Nyberg <johanna@billingskognyberg.se>
2024-03-25 09:05:55 +0100

    Added error messages if input characters are less than 5 or more than 140.

--- src/components/MessageInput.jsx
+++ src/components/MessageInput.jsx
@@ -4,13 +4,24 @@ import "./MessageInput.css";
 
 export const MessageInput = ({ sendMessage }) => {
   const [inputValue, setInputValue] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    if (!inputValue.trim()) return; // Prevent submitting empty messages
-
-    sendMessage(inputValue);
-    setInputValue(""); // Clear input field after submitting
+    if (inputValue.length < 5) {
+      setErrorMessage("Your thought has to be longer than 5 characters.");
+    } else if (inputValue.length > 140) {
+      setErrorMessage(
+        "Your thought is too long. It has to be less than 140 characters."
+      );
+    } else {
+      // If there's no error, clear the error message
+      setErrorMessage("");
+      // Call sendMessage function
+      sendMessage(inputValue);
+      // Clear input field
+      setInputValue("");
+    }
   };
 
   return (
@@ -23,6 +34,7 @@ export const MessageInput = ({ sendMessage }) => {
           value={inputValue}
           onChange={(event) => setInputValue(event.target.value)}
         />
+        {errorMessage && <p className="error-message">{errorMessage}</p>}
         <button className="submit-button" type="submit">
           ❤️
           <p className="button-text"> Send Happy Thought </p>
