0472c38d1814a02539571ba3adcbf08287a7fef8
Your Name <you@example.com>
2024-03-25 21:58:36 +0100

    Add title

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { PostCreator } from "./components/PostCreator"
 import { PostWall } from "./components/PostWall"
 import { Footer } from "./components/Footer"
+import { PostTitle } from "./components/PostTitle"
 
 export const App = () => {
   return (
     <>
+      <PostTitle />
       <PostCreator />
       <PostWall />
       <Footer />
---
+++ src/components/PostTitle.jsx
@@ -0,0 +1,7 @@
+export const PostTitle = () => {
+  return (
+    <div className="title">
+      <span className="title">HAPPY THOUGHTS</span>                                                                      
+    </div>
+  )
+}
