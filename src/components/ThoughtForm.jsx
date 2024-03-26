114de24c20643497de16db3d67a080c47bb5ca4a
Mai Kanetaka <maikanetaka@gmail.com>
2024-03-26 13:17:09 +0100

    Add word count limit

--- src/components/ThoughtForm.jsx
+++ src/components/ThoughtForm.jsx
@@ -3,10 +3,32 @@ import PropTypes from "prop-types";
 import styles from "./ThoughtForm.module.css";
 
 const ThoughtForm = ({ addThought }) => {
+  const maxLength = 140;
   const [message, setMessage] = useState("");
+
+  const handleTextChange = (event) => {
+    setMessage(event.target.value);
+  };
+
+  const getWordCountText = () => {
+    const typedLength = message.length;
+    const remainingLength = maxLength - typedLength;
+
+    let wordCountMessage;
+    if (remainingLength >= 0) {
+      wordCountMessage = `You have typed ${typedLength} letters, you still have ${remainingLength} to go.`;
+    } else {
+      wordCountMessage = `You have typed too many letters. The limit is ${maxLength}.`;
+    }
+    return wordCountMessage;
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
+
+    if (message.length > maxLength) {
+      console.error("Maximum ");
+    }
 
     fetch("https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts", {
       method: "POST",
@@ -31,9 +53,16 @@ const ThoughtForm = ({ addThought }) => {
       <textarea
         className={styles.thoughtInput}
         value={message}
-        onChange={(event) => setMessage(event.target.value)}
+        onChange={handleTextChange}
         placeholder="Type here!"
       ></textarea>
+      <div
+        className={
+          message.length > maxLength ? styles.wordCountOver : styles.wordCount
+        }
+      >
+        {getWordCountText()}
+      </div>
       <button type="submit" className={styles.thoughtSubmit}>
         ❤️ Send Happy Thought ❤️
       </button>
