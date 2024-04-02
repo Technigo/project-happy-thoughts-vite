1066b62cadd581fc8467ac1d591c1ec0bc0f8f13
Alexandra Franzén <156119504+Xikitita@users.noreply.github.com>
2024-04-02 16:48:28 +0200

    Post render (#7)
    * removed all code that doesn't work at the moment, to fix post form render
    
    * Moved all code to App.jsx to fix type form rendering with every post

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,108 @@
 import { useState, useEffect } from "react";
-import { Fetch } from "./components/Fetch";
+import { Thought } from "./components/Thought";
+import { PostForm } from "./components/PostForm";
 
 
 export const App = () => {
   const [fetchThought, setFetchThought] = useState([]);
+  const [getThought, setGetThought] = useState("");
+  const [loadingThoughts, setLoadingThoughts] = useState(true);
+  const [newThought, setNewThought] = useState("");
 
+  const url = "https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts";
+
+  // UseEffect??? 
   useEffect(() => {
-    fetch("https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts")
+    fetch(url)
       .then((res) => res.json())
       .then((json) => {
+        // Vad får jag här?
         setFetchThought(json);
         console.log(json);
       });
+      // [] dependencies
   }, []);
 
+
   useEffect(() => {}, [fetchThought]);
+
+  // GET new thought
+  const fetchMessages = async () => {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      if (data) {
+        setGetThought(data.thoughts);
+      }
+    } catch (error) {
+      console.log("Could not load toughts, try again", error);
+    } finally {
+      setLoadingThoughts(false);
+    }
+  };
+
+
+  const handleNewThoughtChange = (event) => {
+    setNewThought(event.target.value);
+  };
+
+  //POST new thought
+
+  const onFormSubmit = async (event) => {
+    event.preventDefault();
+
+    if (newThought.trim().length < 5 && newThought.trim().length < 140) {
+      alert("Please type a thought between 5 and 140 characters");
+      return;
+    }
+
+    const thought = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        message: newThought,
+      }),
+    };
+    try {
+      const response = await fetch(url, thought);
+
+      if (response.ok) {
+        await fetchMessages();
+      }
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setNewThought("");
+    }
+  };
+
+  useEffect(() => {
+    setTimeout(() => {
+      fetchMessages();
+    }, 1000);
+  }, []);
+
 
   return (
     <div>
       <h1>Happy Thoughts</h1>
+      <div>
+        <PostForm
+          newThought={newThought}
+          onHandleNewThoughtChange={handleNewThoughtChange}
+          onFormSubmit={onFormSubmit}
+        />
+      </div>
       {fetchThought.map((thought) => (
-        <Fetch
+        <Thought
           key={thought._id}
           message={thought.message}
           hearts={thought.hearts}
           time={thought.createdAt}
+          loadingThoughts={loadingThoughts}
+          getThought={getThought}
         />
       ))}
     </div>
