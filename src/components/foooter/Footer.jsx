af75a5b28857d1f88158d6244ada4eb211726a56
hamdiolad <oladhamdi@gmail.com>
2024-04-27 13:04:12 +0200

    pic

--- src/components/foooter/Footer.jsx
+++ src/components/foooter/Footer.jsx
@@ -4,8 +4,11 @@ export const Footer = () => {
   return (
     <div className="footer-container">
       <p>Created by </p>
-      <a href="https://github.com/hamdiolad" target="blank">
-        <img src="src/assets/icons8-github.png" alt="Github icon" />
+      <a
+        href="https://github.com/hamdiolad"
+        target="_blank"
+        rel="noopener noreferrer">
+        <img src="/icons8-github.png" alt="Github icon" />
       </a>
       <p>Hamdi Olad</p>
     </div>
