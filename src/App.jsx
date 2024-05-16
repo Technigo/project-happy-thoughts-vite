f47f484c431fedac3696aaa846e432034bf0a58f
ohitsnathalie <152981339+ohitsnathalie@users.noreply.github.com>
2024-05-16 22:18:26 +0200

    changed API

--- src/App.jsx
+++ src/App.jsx
@@ -4,50 +4,54 @@ import { Form } from "./components/Form.jsx"
 import { Feed } from "./components/Feed.jsx"
 
 export const App = () => {
-	// Define state and default value
-	const [thought, setThought] = useState([])
+  // Define state and default value
+  const [thought, setThought] = useState([])
 
-	// Connect API and fetch data for the feed
-	const API = "https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts"
+  // Connect API and fetch data for the feed
+  const API = "https://project-happy-thoughts-api-d2f4.onrender.com/thoughts"
 
-	const fetchData = async () => {
-		try {
-			const response = await fetch(API)
-			if (!response.ok) {
-				throw new Error("Failed to fetch happy thoughts")
-			}
-			const data = await response.json()
-			setThought(data)
-		} catch (error) {
-			console.error("Error fetching data", error)
-		}
-	}
+  const fetchData = async () => {
+    try {
+      const response = await fetch(API)
+      if (!response.ok) {
+        throw new Error("Failed to fetch happy thoughts")
+      }
+      const data = await response.json()
+      setThought(data)
+    } catch (error) {
+      console.error("Error fetching data", error)
+    }
+  }
 
-	useEffect(() => {
-		fetchData()
-		//Timer to fetch data every 5 seconds
-		const idInterval = setInterval(fetchData, 5000)
-		return () => {
-			clearInterval(idInterval)
-		}
-	}, [])
+  useEffect(() => {
+    fetchData()
+    //Timer to fetch data every 5 seconds
+    const idInterval = setInterval(fetchData, 5000)
+    return () => {
+      clearInterval(idInterval)
+    }
+  }, [])
 
-	const addThought = (newThought) => {
-		const thoughtKey = Date.now() //using time as key
-		const thoughtAndKey = {
-			...newThought,
-			_id: thoughtKey,
-		}
-		setThought([thoughtAndKey, ...thought])
-	}
+  const addThought = (newThought) => {
+    const thoughtKey = Date.now() //using time as key
+    const thoughtAndKey = {
+      ...newThought,
+      _id: thoughtKey,
+    }
+    setThought([thoughtAndKey, ...thought])
+  }
 
-	return (
-		<>
-			<Header />
-			<div className='main-wrapper'>
-				<Form newThought={addThought} apiURL={API} fetchData={fetchData} />
-				<Feed thoughtData={thought} />
-			</div>
-		</>
-	)
+  return (
+    <>
+      <Header />
+      <div className='main-wrapper'>
+        <Form
+          newThought={addThought}
+          apiURL={API}
+          fetchData={fetchData}
+        />
+        <Feed thoughtData={thought} />
+      </div>
+    </>
+  )
 }
--- src/components/SingleThought.jsx
+++ src/components/SingleThought.jsx
@@ -3,65 +3,67 @@ import moment from "moment"
 import "../styling/thought.css"
 
 export const SingleThought = ({ eachThought, onLikeChange }) => {
-	const [like, setLike] = useState(false)
-	const [numberLikes, setNumberLikes] = useState(eachThought.hearts)
+  const [like, setLike] = useState(false)
+  const [numberLikes, setNumberLikes] = useState(eachThought.hearts)
 
-	const LIKE_API = `https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts/${eachThought._id}/like`
+  const LIKE_API = `https://project-happy-thoughts-api-d2f4.onrender.com/thoughts/${eachThought._id}/like`
 
-	useEffect(
-		() => {
-			const likedThought =
-				JSON.parse(localStorage.getItem("likedThought")) || []
-			if (likedThought.includes(eachThought._id)) {
-				setLike(true)
-			}
-		},
-		[eachThought._id],
-		like
-	)
-	const toggleLike = async () => {
-		const option = {
-			method: "POST",
-			headers: { "Content-Type": "application/json" },
-		}
-		try {
-			const response = await fetch(LIKE_API, option)
+  useEffect(
+    () => {
+      const likedThought =
+        JSON.parse(localStorage.getItem("likedThought")) || []
+      if (likedThought.includes(eachThought._id)) {
+        setLike(true)
+      }
+    },
+    [eachThought._id],
+    like
+  )
+  const toggleLike = async () => {
+    const option = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+    }
+    try {
+      const response = await fetch(LIKE_API, option)
 
-			if (response.ok) {
-				const updateLikes = numberLikes + 1
-				setNumberLikes(updateLikes)
-				setLike((prevLike) => !prevLike)
-				onLikeChange(1)
-			} else {
-				const responseData = await response.json()
-				console.error("Error to like thought", response.status)
-				console.error("Response data:", responseData)
-			}
-		} catch (error) {
-			console.error("An unexpected error occurred", error)
-		}
-	}
-	const handleDate = (date) => {
-		const newDate = new Date(date)
-		return moment(newDate).fromNow()
-	}
+      if (response.ok) {
+        const updateLikes = numberLikes + 1
+        setNumberLikes(updateLikes)
+        setLike((prevLike) => !prevLike)
+        onLikeChange(1)
+      } else {
+        const responseData = await response.json()
+        console.error("Error to like thought", response.status)
+        console.error("Response data:", responseData)
+      }
+    } catch (error) {
+      console.error("An unexpected error occurred", error)
+    }
+  }
+  const handleDate = (date) => {
+    const newDate = new Date(date)
+    return moment(newDate).fromNow()
+  }
 
-	return (
-		<div className='single-thought'>
-			<p className='thought-text'>{eachThought.message}</p>
-			<div className='like-time-container'>
-				<div className='btn-count-container'>
-					<button
-						className={`heart-btn ${like ? "liked" : ""}`}
-						onClick={toggleLike}>
-						❤️
-					</button>
-					<p>x {numberLikes}</p>
-				</div>
-				<p className='timestamp' key={eachThought._id}>
-					{handleDate(eachThought.createdAt)}
-				</p>
-			</div>
-		</div>
-	)
+  return (
+    <div className='single-thought'>
+      <p className='thought-text'>{eachThought.message}</p>
+      <div className='like-time-container'>
+        <div className='btn-count-container'>
+          <button
+            className={`heart-btn ${like ? "liked" : ""}`}
+            onClick={toggleLike}>
+            ❤️
+          </button>
+          <p>x {numberLikes}</p>
+        </div>
+        <p
+          className='timestamp'
+          key={eachThought._id}>
+          {handleDate(eachThought.createdAt)}
+        </p>
+      </div>
+    </div>
+  )
 }
