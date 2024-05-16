9c08bf416cd3ef96e1301106c90a246c3100974f
Johanna Billingskog Nyberg <johanna@billingskognyberg.se>
2024-05-16 15:24:05 +0200

    Fixed bug with empty message card showing up on submit.

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -11,23 +11,6 @@ export const Messages = () => {
     fetchMessages();
   }, []);
 
-  // const fetchMessages = () => {
-  //   fetch("https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts")
-  //     .then((response) => {
-  //       if (!response.ok) {
-  //         throw new Error("- Could not load thoughts -");
-  //       }
-  //       return response.json();
-  //     })
-  //     .then((data) => {
-  //       setMessages(data.slice(0, 20));
-  //       setError(null); // Clear error if fetch is successful
-  //     })
-  //     .catch((error) => {
-  //       setError(error.message); // Set error message in state
-  //     });
-  // };
-
   const fetchMessages = () => {
     fetch("https://happy-thoughts-api-igwpvuz3lq-lz.a.run.app/thoughts")
       .then((response) => {
@@ -45,28 +28,6 @@ export const Messages = () => {
       });
   };
 
-  // const sendMessage = (inputValue) => {
-  //   fetch("https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts/", {
-  //     method: "POST",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     },
-  //     body: JSON.stringify({ message: inputValue }),
-  //   })
-  //     .then((response) => {
-  //       if (!response.ok) {
-  //         throw new Error("- Could not send your thought -");
-  //       }
-  //       return response.json();
-  //     })
-  //     .then((newMessage) => {
-  //       setMessages([newMessage, ...messages.slice(0, 19)]);
-  //     })
-  //     .catch((error) => {
-  //       setError(error.message); // Set error message in state
-  //     });
-  // };
-
   const sendMessage = (inputValue) => {
     fetch("https://happy-thoughts-api-igwpvuz3lq-lz.a.run.app/thoughts", {
       method: "POST",
@@ -81,8 +42,8 @@ export const Messages = () => {
         }
         return response.json();
       })
-      .then((newMessage) => {
-        setMessages([newMessage, ...messages]);
+      .then(() => {
+        fetchMessages();
       })
       .catch((error) => {
         setError(error.message); // Set error message in state
@@ -122,59 +83,7 @@ export const Messages = () => {
         revertedMessages[index].hearts -= 1;
         setMessages(revertedMessages);
       });
-
-    // const handleHeartClick = (index) => {
-    //   // Increment hearts count for the selected message
-    //   const updatedMessages = [...messages];
-    //   updatedMessages[index].hearts += 1;
-    //   setMessages(updatedMessages);
-
-    //   // Extract the _id of the message
-    //   const messageId = messages[index]._id;
-
-    //   fetch(
-    //     `https://happy-thoughts-api-igwpvuz3lq-lz.a.run.app/thoughts/${messageId}/like`,
-
-    //     {
-    //       method: "PATCH",
-    //       headers: {
-    //         "Content-Type": "application/json",
-    //       },
-    //     }
-    //   )
-    //     .then((response) => {
-    //       if (!response.ok) {
-    //         throw new Error("Failed to send your heart");
-    //       }
-    //     })
-    //     .catch((error) => {
-    //       console.error("Error updating hearts count:", error);
-    //       setError(error.message); // Set error message in state
-    //     });
   };
-
-  // Call API to update hearts count
-  //   fetch(
-  //     `https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts/${messageId}/like`,
-
-  //     {
-  //       method: "PATCH",
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //       },
-  //       body: JSON.stringify({ messageId }), // Include message ID in the body
-  //     }
-  //   )
-  //     .then((response) => {
-  //       if (!response.ok) {
-  //         throw new Error("Failed to send your heart");
-  //       }
-  //     })
-  //     .catch((error) => {
-  //       console.error("Error updating hearts count:", error);
-  //       setError(error.message); // Set error message in state
-  //     });
-  // };
 
   return (
     <div className="App">
