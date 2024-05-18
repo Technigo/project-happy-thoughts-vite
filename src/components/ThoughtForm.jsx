905463f70659c06af1a5d471797456cee56e35bc
Wendy Zhao <wendy.w.zhao@outlook.com>
2024-05-18 18:28:01 +0200

    Use useRef instead of useState in the form component

--- src/components/ThoughtForm.jsx
+++ src/components/ThoughtForm.jsx
@@ -1,18 +1,34 @@
-import PropTypes from "prop-types";
+import { useRef } from "react";
+
+import { useThoughtStore } from "../stores/useThoughtStore";
 import styles from "./ThoughtForm.module.css";
 
-const ThoughtForm = ({ input, onSubmit, onChange }) => {
-  const charCount = input.trim().length;
+const ThoughtForm = () => {
+  const { postThought, setError } = useThoughtStore();
+  const inputRef = useRef("");
+  const charCount = inputRef.current.value?.trim().length;
+
+  // Post request on form submit, also validate the input
+  const createThought = event => {
+    event.preventDefault();
+    const message = inputRef.current.value;
+    if (message.trim().length >= 5 && message.trim().length <= 140) {
+      postThought(message);
+      inputRef.current.value = "";
+    } else {
+      setError("Input invalid: You must type within 5 to 140 words");
+    }
+  };
   return (
-    <form className={styles.form} onSubmit={onSubmit}>
+    <form className={styles.form} onSubmit={createThought}>
       <p className={styles.title}>What&apos;s making you happy right now?</p>
       <label htmlFor="message"></label>
       <textarea
         className={styles.input}
         id="message"
         name="message"
-        value={input}
-        onChange={onChange}
+        ref={inputRef}
+        onChange={() => setError()}
         placeholder="Type your happy thought here!"
       />
       <p
@@ -33,9 +49,3 @@ const ThoughtForm = ({ input, onSubmit, onChange }) => {
 };
 
 export default ThoughtForm;
-
-ThoughtForm.propTypes = {
-  input: PropTypes.string.isRequired,
-  onSubmit: PropTypes.func.isRequired,
-  onChange: PropTypes.func.isRequired,
-};
--- src/components/ThoughtsCollection.jsx
+++ src/components/ThoughtsCollection.jsx
@@ -1,5 +1,5 @@
 import Lottie from "lottie-react";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
 import emptySearch from "../assets/emptySearch.json";
 import loadingAnimation from "../assets/loading_animation.json";
@@ -16,45 +16,20 @@ const ThoughtsCollection = () => {
     loading,
     error,
     fetchThoughts,
-    postThought,
-    setError,
     filter,
     likedThoughts,
     sentThoughts,
   } = useThoughtStore();
-  const [message, setMessage] = useState("");
-
-  //reset the error and record the value of text input
-  const handleInputChange = event => {
-    setError();
-    const userInput = event.target.value;
-    setMessage(userInput);
-  };
 
   //useEffect to render API data on mount
   useEffect(() => {
     fetchThoughts();
   }, [fetchThoughts]);
 
-  // Post request on form submit, also validate the input
-  const createThought = event => {
-    event.preventDefault();
-    if (message.trim().length >= 5 && message.trim().length <= 140) {
-      postThought(message);
-      setMessage("");
-    } else {
-      setError("Input invalid: You must type within 5 to 140 words");
-    }
-  };
-
   return (
     <div>
       <HandleError error={error} />
-      <ThoughtForm
-        onSubmit={createThought}
-        input={message}
-        onChange={handleInputChange}
-      />
+      <ThoughtForm />
       {thoughts && <Counter />}
       <div className={styles.thoughts}>
         {
@@ -75,10 +50,7 @@ const ThoughtsCollection = () => {
               ? sentThoughts
               : thoughts
             ).map(thought => (
-              <ThoughtCard
-                thought={thought}
-                key={thought._id}
-              />
+              <ThoughtCard thought={thought} key={thought._id} />
             ))
         }
         {loading && <Lottie animationData={loadingAnimation} loop={true} />}
