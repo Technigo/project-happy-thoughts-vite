b7b4f8f6d1953431c77797a19049172b4d23b225
Your Name <you@example.com>
2024-05-20 11:31:27 +0200

    add /thoughts at the end of the url

--- src/components/PostWall.jsx
+++ src/components/PostWall.jsx
@@ -9,7 +9,7 @@ export const PostWall  = () => {
   const [likesPerClick, setLikesPerClick] = useState(1)
   
   // My own server
-  const url = "https://arnaus-happy-thoughts-api.onrender.com"
+  const url = "https://arnaus-happy-thoughts-api.onrender.com/thoughts"
 
   //Technigo server
   //const url = "https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts"
