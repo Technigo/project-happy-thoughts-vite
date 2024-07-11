bf30aa210a879f857add766f5b28509ac94464b1
Alma Herrstrom <almaherrstrom@hotmail.com>
2024-07-11 18:39:58 +0200

    added a loading state and lottie

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,30 @@
 import { useEffect, useState } from "react"
 import { Feed } from "./components/Feed"
 import { Form } from "./components/Form"
+import Lottie from "lottie-react"
+import animationData from "./assets/lottie-loading.json"
 import "./App.css"
 
 export const App = () => {
   //Initialize state for storing dafault data
   const [thoughts, setThoughts] = useState([])
+  const [loading, setLoading] = useState(true)
 
   //Define API endpoints
   const thoughtsURL = "https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts"
 
   //Function to fetch data from API
   const fetchHappyThoughts = () => {
+    setLoading(true)
     fetch(thoughtsURL)
       .then((result) => result.json())
       .then((json) => {
         setThoughts(json)
+        setLoading(false)
       })
       .catch((error) => {
         console.error("Error fetching thoughts:", error)
+        setLoading(false)
       })
   }
 
@@ -65,12 +71,12 @@ export const App = () => {
         {/* Pass handleFormSubmit function as prop so I can use it in Form*/}
         <Form handleFormSubmit={handleFormSubmit} />
         <div className="feed">
-          {thoughts.length > 0 ? (
-            <Feed thoughts={thoughts} fetchHappyThoughts={fetchHappyThoughts} />
-          ) : (
-            <div className="loading-text">
-              <p>❤️ Be patient, we&apos;re fetching happy thoughts... ❤️</p>
+          {loading ? (
+            <div className="lottie-container">
+              <Lottie className="lottie" animationData={animationData} />
             </div>
+          ) : (
+            <Feed thoughts={thoughts} fetchHappyThoughts={fetchHappyThoughts} />
           )}
         </div>
       </div>
