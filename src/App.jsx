04079bca0867080facf1a92f557e914b501b553b
Jonash189 <jonas.hellstrom89@gmail.com>
2024-10-23 12:23:35 +0200

    Creating first component HappyToughts were i will store everyting.

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import React from "react";
+
+
 export const App = () => {
-  return <div>Find me in src/app.jsx!</div>;
+  return <div>hejhej</div>;
 };
---
+++ src/components/HappyThoughts.jsx
@@ -0,0 +1,8 @@
+
+import React from "react";
+
+export const HappyThoughts = () => {
+    const [thoughts, setThoughts] = useState([])
+    const [newthoughts, setNewThoughts] = useState("")
+
+}
