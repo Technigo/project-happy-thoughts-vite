fddcaf8710a5bb59b2a2d63c23ffd9b98da7e5a0
iamfannyhenriques <fanny.henriques@hotmail.com>
2024-10-23 20:47:58 +0200

    started with project happy thoughts - fetched messages from URL

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,37 @@
+
+import { useState, useEffect } from 'react'
+
+const BASE_URL = "https:happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts";
+
 export const App = () => {
-  return <div>Find me in src/app.jsx!</div>;
+  const [happyThought, setHappyThought] = useState([]);
+
+  useEffect(() => {
+    const fetchHappyThought = async () => {
+      try {
+        const response = await fetch(BASE_URL);
+        const data = await response.json();
+        const message = data.map(thought => thought.message);
+        setHappyThought(message);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    fetchHappyThought();
+  }, []);
+
+
+
+  return (
+    <div>
+      <ul>
+        {happyThought.length > 0
+          ? happyThought.map((message, index) => (
+            <li key={index}>{message}</li>
+          ))
+          : "Loading..."}
+      </ul>
+    </div>
+  );
 };
+
