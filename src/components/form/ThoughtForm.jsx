c213610fbc9e53307942bfa22aee8764ff23bd78
Gitte Beckmann <gittebeckmann@hotmail.com>
2024-10-25 10:41:06 +0200

    - added a submit button component, - changed input field into textarea - styled textarea

--- src/components/form/ThoughtForm.jsx
+++ src/components/form/ThoughtForm.jsx
@@ -1,17 +1,21 @@
 import PropTypes from "prop-types"
 import "./form.css"
+import { SubmitButton } from "../submitButton/SubmitButton"
 
 export const ThoughtsForm = ({ newThought, setNewThought, handleFormSubmit }) => {
     return (
         <div className="form-container">
             <p>What is making you happy right now?</p>
             <form onSubmit={handleFormSubmit}>
-                <input
+                <textarea
                     type="text"
                     value={newThought}
-                    onChange={(e) => setNewThought(e.target.value)} placeholder="Type here...">
-                </input>
-                <button type="submit">Submit</button>
+                    onChange={(e) => setNewThought(e.target.value)}
+                    placeholder="Type here..."
+                    maxLength="140">
+                </textarea>
+                <SubmitButton />
+                {/* <button type="submit">Submit</button> */}
             </form>
         </div>
     )
---
+++ src/components/submitButton/SubmitButton.jsx
@@ -0,0 +1,8 @@
+import "./submitButton.css"
+
+export const SubmitButton = () => {
+    return (
+        <button type="submit">❤️ Send Happy Thought ❤️</button>
+
+    )
+}
