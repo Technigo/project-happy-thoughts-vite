0a3970bdb3ec3b48a4fe5c2858369db1523fae08
Anna <annah80@gmail.com>
2024-10-29 14:23:51 +0100

    added hover effect to button

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,10 @@
 import "../Styles/Header.css"
-import smilingHeart2 from "../assets/smilingheart2.png";
+import smilingHeart from "../assets/smilingheart.png";
 
 export const Header = () => {
   return (
     <header>
-      <img src={smilingHeart2} alt="Smiling heart image" className="smiling-heart-icon" />
+      <img src={smilingHeart} alt="Smiling heart image" className="smiling-heart-icon" />
       <h1>&quot;The happiness of your life depends on the quality of your thoughts&quot;</h1>
       <p className="quote">- Marcus Aurelius</p>
     </header>
