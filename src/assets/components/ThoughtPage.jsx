72f5c57f23769603d31710061abee9248125ecf8
iamfannyhenriques <fanny.henriques@hotmail.com>
2024-12-19 17:52:39 +0100

    updated url to the mongoURL

--- src/assets/components/ThoughtPage.jsx
+++ src/assets/components/ThoughtPage.jsx
@@ -5,70 +5,72 @@ import { PostThought } from "./PostThought"
 
 export const ThoughtPage = () => {
   const BASE_URL = "https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts";
+  //"https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts";
   const LIKE_URL = "https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts/THOUGHT_ID/like";
+  //"https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts/THOUGHT_ID/like";
 
   //state for fetching
   const [happyThoughts, setHappyThoughts] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
-//Fetching the data from the API
-useEffect(() => {
-  const fetchHappyThoughts = async () => {
+  //Fetching the data from the API
+  useEffect(() => {
+    const fetchHappyThoughts = async () => {
+      try {
+        const response = await fetch(BASE_URL);
+        const data = await response.json();
+        setHappyThoughts(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchHappyThoughts();
+  }, []);
+
+  //POSTCALL for API
+  const postHappyThought = async (thought) => {
     try {
-      const response = await fetch(BASE_URL);
+      const response = await fetch(BASE_URL, {
+        method: "POST",
+        body: JSON.stringify({
+          message: thought,
+        }),
+        headers: { "Content-Type": "application/json" },
+      });
       const data = await response.json();
-      setHappyThoughts(data);
+      const newThought = data;
+      setHappyThoughts((previousThoughts) => [newThought, ...previousThoughts]);
+
     } catch (error) {
-      console.error("Error fetching data:", error);
-    } finally {
-      setIsLoading(false);
+      console.error("Error posting data:", error);
     }
   };
-  fetchHappyThoughts();
-}, []);
 
-//POSTCALL for API
-const postHappyThought = async (thought) => {
-  try {
-    const response = await fetch(BASE_URL, {
-      method: "POST",
-      body: JSON.stringify({
-        message: thought,
-      }),
-      headers: { "Content-Type": "application/json" },
-    });
-    const data = await response.json();
-    const newThought = data;
-    setHappyThoughts((previousThoughts) => [newThought, ...previousThoughts]);
+  //postcall for likes on thoughts
+  const postLike = async (thoughtId) => {
+    try {
+      const response = await fetch(LIKE_URL.replace("THOUGHT_ID", thoughtId), {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+      });
+      const data = await response.json();
+      const existingThoughts = [...happyThoughts];
+      const existingThought = existingThoughts.find(
+        thought => thought._id === thoughtId
+      );
+      existingThought.hearts = data.hearts;
+      setHappyThoughts(existingThoughts);
+    } catch (error) {
+      console.error("Error posting data:", error);
+    }
+  };
 
-  } catch (error) {
-    console.error("Error posting data:", error);
-  }
-};
-
-//postcall for likes on thoughts
-const postLike = async (thoughtId) => {
-  try {
-    const response = await fetch(LIKE_URL.replace("THOUGHT_ID", thoughtId), {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-    });
-    const data = await response.json();
-    const existingThoughts = [...happyThoughts];
-    const existingThought = existingThoughts.find(
-      thought => thought._id === thoughtId
-    );
-    existingThought.hearts = data.hearts;
-    setHappyThoughts(existingThoughts);
-  } catch (error) {
-    console.error("Error posting data:", error);
-  }
-};
-
-return (
-  <>
-    <PostThought postHappyThought={postHappyThought} />
-    <DisplayThoughts happyThoughts={happyThoughts} isLoading={isLoading} postLike={postLike} />
-  </>
-);
+  return (
+    <>
+      <PostThought postHappyThought={postHappyThought} />
+      <DisplayThoughts happyThoughts={happyThoughts} isLoading={isLoading} postLike={postLike} />
+    </>
+  );
 };
