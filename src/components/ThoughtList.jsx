0581830789ebd7fa843675c3803e4e7b2f540b28
Jonash189 <jonas.hellstrom89@gmail.com>
2024-12-19 20:58:47 +0100

    .

--- src/components/ThoughtList.jsx
+++ src/components/ThoughtList.jsx
@@ -6,7 +6,7 @@ import { ThoughtLikes } from "./ThoughtLikes.jsx";
 export const ThoughtList = ({ thoughts }) => {
   return (
     <section className="thought-section">
-      {thoughts.map((thought) => (
+      {Array.isArray(thoughts) && thoughts.map((thought) => (
         <div className="thought-wrapper" key={thought._id}>
           <div className="input-message">{thought.message}</div>
           <div className="info-wrapper">
