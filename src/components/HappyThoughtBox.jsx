003307c466532560fce1e97a8a34d284739fa0fb
Erika Olsson <erikamolsson87@gmail.com>
2024-12-22 22:14:06 +0100

    update: spelled wrong i new link fixed

--- src/components/HappyThoughtBox.jsx
+++ src/components/HappyThoughtBox.jsx
@@ -13,7 +13,7 @@ const HappyThoughtBox = (props) => {
     /* Fetches the data from the API/url */
     useEffect(() => {
         /* old api: https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts */
-        fetch("https://project-19-happy-thoughts-api.onrender.com/happythougts")
+        fetch("https://project-19-happy-thoughts-api.onrender.com/happythoughts")
             .then(response => response.json())
             .then(data => setHappyFeed(data.slice(0, 20))) // Limit to 20 latest items
             .catch(error => console.error('Error fetching data:', error));
