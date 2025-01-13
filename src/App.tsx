4ab065bf3d01e1df97d493e4bffe167427159045
JoyceKuode <joyce.kuo@me.com>
2025-01-13 12:15:33 +0100

    Update fetch URL

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import SubmitForm from "./components/SubmitForm";
 import { HappyThought } from "./components/HappyThought";
 
-const URL = "https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts";
+const URL = "https://project-happy-thoughts-api-ph1w.onrender.com/";
 
 export interface Thought {
   _id: string;
